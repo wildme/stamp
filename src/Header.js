f9a572d619f14887b22934968dd93051a4e5a740
Konstantin Ashaev <techno_nix@mail.ru>
2021-03-29 21:58:12 +0300

    Edited Header section

--- src/Header.js
+++ src/Header.js
@@ -1,13 +1,11 @@
-import logo from './logo.jpg';
 import './App.css';
 
 export default function Header() {
     return (
         <header className="App-header">
-            <img scr={logo} className="App-logo" alt="logo" />
                 <nav>
-                    <a href="#s">Inbox</a>
-                    <a href="#s">Outbox</a>
+                    <a href="#s">Inbox</a> | 
+                    <a href="#s">Outbox</a> | 
                 </nav> 
         </header>
     );
