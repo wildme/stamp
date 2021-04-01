9d5f685d3e5ba873ed5cb3b598e313ee203ec78b
Konstantin Ashaev <kostya@gorgaz.ru>
2021-04-01 15:46:00 +0300

    Add new routes

--- src/Content.js
+++ src/Content.js
@@ -1,4 +1,6 @@
 import './App.css';
+import { InboxHeader, InboxMain } from './Main.js';
+import Login from './Login.js';
 import {
     BrowserRouter as Router,
     Route,
@@ -15,53 +17,8 @@ export default function Content() {
          </header>
          <main>
           <Route exact path="/inbox" component={InboxMain} />
+          <Route exact path="/logout" component={Login} />
          </main>
         </Router>
-    );
-}
-
-function InboxHeader() {
-    return <h3>Inbox</h3>
-}
-
-function InboxMain() {
-    return (
-        <div className="main">
-          <div className="panel">
-            <button>New</button>
-            <button>Edit</button>
-          </div>
-          <div className="mainTable">
-            <table>
-              <tr id="topRow">
-                <th>#</th>
-                <th>Subject</th>
-                <th>Sender</th>
-                <th>Date</th>
-                <th>User</th>
-                <th>Note</th>
-                <th>File</th>
-              </tr>
-              <tr>
-                <td>1</td>
-                <td>Test</td>
-                <td>Big Corp.</td>
-                <td>Konstantin</td>
-                <td>1-01-2021</td>
-                <td>test test</td>
-                <td>Document.doc</td>
-              </tr>
-              <tr>
-                <td>2</td>
-                <td>Test2</td>
-                <td>Ajax</td>
-                <td>Konstantin</td>
-                <td>1-01-2021</td>
-                <td>test2 test</td>
-                <td>Document.docx</td>
-              </tr>
-            </table>
-          </div>  
-          </div>
     );
 }
