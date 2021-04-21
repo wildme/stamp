73caae4c17ecf722d2795c75e2ba4548bb32ff04
Konstantin Ashaev <techno_nix@mail.ru>
2021-04-21 22:50:45 +0300

    Add indents for readability

--- src/Header.js
+++ src/Header.js
@@ -1,16 +1,14 @@
 import './App.css';
 
-export default function Header() {
+export default function Header(props) {
     return (
         <div className="navbar">
-           <a href="/inbox">Inbox</a> 
-           <a href="/outbox">Outbox</a>
-           <div class="navbar-right">
-                <a href="/logout">Logout</a>
-                <a href="/login">Login</a>
-           </div>
-            
+            <a href="/inbox">Inbox</a> 
+            <a href="/outbox">Outbox</a>
+                <div className="navbar-right">
+                    <a href="/logout">Logout</a>
+                    <a href="/login">Login</a>
+                </div>
         </div>
     );
 }
-    
