5b37f649452cf1c60373c129ac5fb817e3dfa0c4
Konstantin Ashaev <kostya@gorgaz.ru>
2021-04-23 15:51:45 +0300

    Add conditional rendering

--- src/Header.js
+++ src/Header.js
@@ -1,14 +1,33 @@
 import './App.css';
+import { connect } from 'react-redux';
 
-export default function Header(props) {
-    return (
+const HeaderLinks = () => {
+    return  (
         <div className="navbar">
             <a href="/inbox">Inbox</a> 
             <a href="/outbox">Outbox</a>
                 <div className="navbar-right">
                     <a href="/logout">Logout</a>
-                    <a href="/login">Login</a>
                 </div>
         </div>
     );
 }
+const HeaderBlank = () => {
+    return (
+        <div className="navbar">
+                <div className="navbar-right">
+                </div>
+        </div>
+    );
+}
+
+const Header = ({user}) => {
+    return user ? <HeaderLinks/> : <HeaderBlank/>
+        
+}
+const mapStateToProps = (state) => {
+    const { user } = state;
+    return { user: user }
+}
+
+export default connect(mapStateToProps)(Header);
