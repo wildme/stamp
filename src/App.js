b02f76d423f01e59a80a20e043db9fbee7e6dfbc
Konstantin Ashaev <kostya@gorgaz.ru>
2021-04-23 15:43:41 +0300

    Add conditional rendering

--- src/App.js
+++ src/App.js
@@ -2,13 +2,22 @@ import './App.css';
 import Header from './Header.js';
 import Content from './Content.js';
 import Footer from './Footer.js';
+import Login from './Login.js';
 
-export default function App() {
+import { connect } from 'react-redux';
+
+const App = ({user}) => {
   return (
       <div className="main">
         <Header />
-        <Content />
+      {user ? <Content /> : <Login />}
         <Footer />
       </div>
   );
 }
+
+const mapStateToProps = (state) => {
+    const { user } = state;
+    return { user: user }
+}
+export default connect(mapStateToProps)(App);
