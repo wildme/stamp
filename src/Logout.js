f233f22285a613c403e180a862769f0d7c035289
Konstantin Ashaev <techno_nix@mail.ru>
2021-04-26 21:15:09 +0300

    New module file

---
+++ src/Logout.js
@@ -0,0 +1,16 @@
+import { connect } from 'react-redux';
+import { logout } from './redux/actions.js';
+import { Fragment } from 'react';
+import { Route, Redirect } from "react-router-dom";
+
+const Logout = ({dispatch}) => {
+    dispatch(logout());
+    return (
+        <Fragment>
+            <Route exact path="/logout" render={() => <Redirect to="/"/>} />
+        </Fragment>
+    );
+};
+
+export default connect()(Logout);
+
