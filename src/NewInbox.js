53812af5705f0ac03e4cf7e0018537ccf396b1f5
Konstantin Ashaev <kostya@gorgaz.ru>
2021-04-27 14:24:27 +0300

    Removes some <input> definitions

--- src/NewInbox.js
+++ src/NewInbox.js
@@ -1,28 +1,64 @@
+import React, { useState } from 'react';
+import { inbox_add } from './redux/actions.js';
 import { connect } from 'react-redux';
 
-const NewInbox = () => {
+const NewInbox = ({user, dispatch}) => {
+    const [subj, setSubj] = useState("");
+    const [sender, setSender] = useState("");
+    const date = new Date().toISOString();
+    const added = user.name;
+    const [note, setNote] = useState("");
+    
+    const handleAddInbox = (e) => {
+        e.preventDefault();
+        dispatch(
+            inbox_add({
+                subj: subj,
+                sender: sender,
+                date: date,
+                added: added,
+                note: note
+            })
+        );
+        setSubj("");
+        setSender("");
+        setNote("");
+    };
+
     return (
         <div className="addRecord">
           <label for="subj"><b>Subject</b></label>
-          <input type="text" name="subj"/>
+          <input
+            type="text"
+            name="subj"
+            value={subj}
+            onChange={(e) => setSubj(e.target.value)}
+        />
 
           <label for="from"><b>Sender</b></label>
-          <input type="text" name="from"/>
+          <input
+            type="text"
+            name="from"
+            value={sender}
+            onChange={(e) => setSender(e.target.value)}
+        />
 
-          <label for="date"><b>Date</b></label>
-          <input type="text" name="date"/>
-
-          <label for="user"><b>User</b></label>
-          <input type="text" name="user"/>
-          
           <label for="note"><b>Note</b></label>
-          <input type="text" name="Note"/>
+          <input
+            type="text"
+            name="note"
+            value={note}
+            onChange={(e) => setNote(e.target.value)}
+        />
 
-          <label for="file"><b>File</b></label>
-          <input type="text" name="file"/>
-        
-          <button type="submit" id="submit">Add</button>
+          <button type="submit" onClick={(e) => handleAddInbox(e)} >Add</button>
         </div>
     );
 }
-export default connect()(NewInbox);
+
+const mapStateToProps = (state) => {
+    const { user } = state;
+    return { user: user }
+}
+
+export default connect(mapStateToProps)(NewInbox);
