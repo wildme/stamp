7e627d06b80d99b91dad39f50b7dfc07f3226560
Konstantin Ashaev <kostya@gorgaz.ru>
2021-04-27 14:18:37 +0300

    Add a reducer for INBOX_ADD

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -1,6 +1,6 @@
-import { LOGIN, LOGOUT } from './actionTypes';
+import { LOGIN, LOGOUT, INBOX_ADD, OUTBOX_ADD } from './actionTypes';
 
-const initialState = { user: null };
+const initialState = { user: null, inbox: [], outbox: [] };
 
 const rootReducer = (state = initialState, action) => {
     switch (action.type) {
@@ -9,8 +9,15 @@ const rootReducer = (state = initialState, action) => {
             return { ...state, user };
         }
         case LOGOUT: {
-            const { user } = action.payload;
-            return { ...state, user };
+            const [ user, inbox, outbox ] = action.payload;
+            return { ...state, user, inbox, outbox };
+        }
+        case INBOX_ADD: {
+            const { id, content } = action.payload;
+            return {
+                ...state,
+                inbox: {...state.inbox, [id]: { content }}
+            }
         }
         default:
             return state;
