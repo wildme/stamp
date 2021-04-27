7b9fbcff508514f74fead1257aaa305a13868731
Konstantin Ashaev <kostya@gorgaz.ru>
2021-04-27 14:16:41 +0300

    Add inbox_add and outbox_add actions

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,4 +1,7 @@
-import { LOGIN, LOGOUT } from './actionTypes';
+import { LOGIN, LOGOUT, INBOX_ADD, OUTBOX_ADD } from './actionTypes';
+
+let nextInboxID = 0;
+let nextOutboxID = 0;
 
 export const login = (user) => ({
     type: LOGIN,
@@ -7,7 +10,15 @@ export const login = (user) => ({
 
 export const logout = () => ({
     type: LOGOUT,
-    payload: { user: null }
+    payload: { user: null, inbox: null, outbox: null }
 });
 
+export const inbox_add = (content) => ({
+    type: INBOX_ADD,
+    payload: { id: ++nextInboxID, content }
+});
 
+export const outbox_add = (content) => ({
+    type: OUTBOX_ADD,
+    payload: { id: ++nextOutboxID, content }
+});
