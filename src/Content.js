c9445c6b5284f8b992a2d5b35e78bc1b3f84e743
Konstantin Ashaev <kostya@gorgaz.ru>
2021-04-28 08:49:37 +0300

    Headings definitions inside <Route>

--- src/Content.js
+++ src/Content.js
@@ -1,18 +1,18 @@
 import './App.css';
+import { connect } from 'react-redux';
 import { Fragment } from 'react';
-import {InboxHeader, OutboxHeader, InboxMain } from './Main.js';
-import Hello from './Hello.js';
+import { InboxMain } from './Main.js';
 import NewInbox from './NewInbox.js';
 import Logout from './Logout.js';
 import { Route } from "react-router-dom";
 
-const Content = () => {
+const Content = ({user}) => {
     return(
         <Fragment>
          <header>
-          <Route exact path="/" component={Hello} />
-          <Route exact path="/inbox" component={InboxHeader} />
-          <Route exact path="/outbox" component={OutboxHeader} />
+          <Route exact path="/" render={() => <h1>Hello, {user.name}</h1>} />
+          <Route exact path="/inbox" render={() => <h2>Inbox</h2>} />
+          <Route exact path="/outbox"  render={() => <h2>Outbox</h2>} />
          </header>
          <main>
           <Route exact path="/inbox" component={InboxMain} />
@@ -22,4 +22,9 @@ const Content = () => {
         </Fragment>
     );
 }
-export default Content;
+
+const mapStateToProps = (state) => {
+    const { user } = state;
+    return { user: user }
+}
+export default connect(mapStateToProps)(Content);
