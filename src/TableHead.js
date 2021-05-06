632ea5e7e3f1214ebfcde686e78732da258e065f
Konstantin Ashaev <kostya@gorgaz.ru>
2021-05-06 16:15:32 +0300

    New file

---
+++ src/TableHead.js
@@ -0,0 +1,36 @@
+export const headInbox = [
+    {
+        id: 'number',
+        label: '#',
+        sortable: true,
+    },
+    {
+        id: 'subject',
+        label: 'Subject',
+        sortable: false,
+    },
+    {
+        id: 'sender',
+        label: 'Sender',
+        sortable: true,
+    },
+
+    {
+        id: 'date',
+        label: 'Date',
+        sortable: false,
+    },
+
+    {
+        id: 'user',
+        label: 'User',
+        sortable: false,
+    },
+
+    {
+        id: 'note',
+        label: 'Note',
+        sortable: false,
+    },
+
+];
