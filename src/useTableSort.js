78d580e06fddd5a5537ce1fcf4d926d35ae74242
Konstantin Ashaev <kostya@gorgaz.ru>
2021-05-14 16:21:11 +0300

    Add functions sorting data

--- src/useTableSort.js
+++ src/useTableSort.js
@@ -1,9 +1,44 @@
 import { useState } from 'react';
 
-const useTableSort = (data = []) => {
+const useTableSort = (data = [1, 2, 3]) => {
     const [sortColumn, setSortColumn] = useState("");
     const [sortDirection, setSortDirection] = useState("");
     const [tableData, setTableData] = useState(data);
+
+    const sortNumber = (numbers = [], column) => 
+        numbers.sort((a, b) => a[column] - b[column]);
+
+    const sortString = (strings = [], column) => 
+        strings.sort((a, b) => {
+            const stringA = a[column].toUpperCase();
+            const stringB = b[column].toUpperCase();
+            if (stringA < stringB) {
+                return -1;
+            }
+            if (stringA > stringB) {
+                return 1;
+            }
+            return 0;
+        });
+
+    const sortData = (column, direction) => {
+        const dataType = typeof tableData[0][column];
+        let sortResult = [];
+        let result = [];
+        if (dataType === "string") {
+            sortResult = sortString(tableData, column);
+            result = direction === 'desc'
+                ? sortResult.reverse()
+                : sortResult;
+            setTableData(result);
+                                                
+        }
+
+        if (dataType === "number") {
+            return sortResult = sortNumber(tableData, column);
+        }
+        else { return tableData }
+    };
 
     const handleSort = (column) => {
         let direction = 'asc';
@@ -12,9 +47,10 @@ const useTableSort = (data = []) => {
         }
         setSortColumn(column);
         setSortDirection(direction);
+        return sortData(column, direction);
     };
 
-    return { handleSort, sortColumn, sortDirection };
+    return { handleSort, sortColumn, sortDirection, tableData };
 }
 
 export default useTableSort;
