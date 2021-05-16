30e28ae8617c9c395abebc8c924a1678b9232035
Konstantin Ashaev <techno_nix@mail.ru>
2021-05-16 21:49:00 +0300

    Add an import of RowActions and respective component

--- src/Inbox.js
+++ src/Inbox.js
@@ -1,11 +1,14 @@
+import RowActions from './RowActions.js';
+
 const Inbox = ({entry}) => (
-    <tr>
+    <tr className="inbox-item">
         <td>{entry.id}</td>
         <td>{entry.subj}</td>
         <td>{entry.sender}</td>
         <td>{entry.date}</td>
         <td>{entry.added}</td>
         <td>{entry.note}</td>
+        <td><RowActions props={entry.id} /></td>
     </tr>
 );
 
