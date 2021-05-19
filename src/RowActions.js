0e59927c88672470ebb7a36922c9555b60a51aa2
Konstantin Ashaev <kostya@gorgaz.ru>
2021-05-19 16:24:26 +0300

    Add hooks to hide/display an action menu

--- src/RowActions.js
+++ src/RowActions.js
@@ -1,22 +1,25 @@
 import { useState } from 'react';
+import { NavLink } from 'react-router-dom';
+
 const RowActions = ({props}) => {
-    const handleClickKebab = (e) => {
+    const [hidden, setHidden] = useState(true);
+    const handleClick = (e) => {
         e.preventDefault();
-        return;
-    }
+        setHidden(!hidden);
+    };
     return (
         <div className="dropdown">
             <div className="kebab-icon">
-                <a href="#menu" onClick={(e) => handleClickKebab(e)}>
+                <a href="#"  onClick={(e) => handleClick(e)} >
                     <svg id="menu" width="17" height="17" xmlns="http://www.w3.org/2000/svg">
                         <circle cx="15%" cy="3" r="2" fill="black"/>
                         <circle cx="15%" cy="9" r="2" fill="black"/>
                         <circle cx="15%" cy="15" r="2" fill="black"/>
                     </svg>
                 </a>
-                    <div className="dropdown-content">
+                    <div className={ hidden ? "dropdown-content-hidden" : "dropdown-content" }>
                         <ul id="row-dropdown" >
-                            <li><a href="#edit">Edit</a></li>
+                            <li><NavLink to="/inbox/new">Edit</NavLink></li>
                         </ul>
                     </div>
             </div>
