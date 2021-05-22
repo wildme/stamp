683afe30736f78f6010fd2c3bab8d1c0d46aabcb
Konstantin Ashaev <techno_nix@mail.ru>
2021-05-22 23:18:52 +0300

    Event handler processes outside clicks

--- src/RowActions.js
+++ src/RowActions.js
@@ -1,24 +1,42 @@
-import { useState } from 'react';
+import { useState, useRef, useEffect } from 'react';
 import { NavLink } from 'react-router-dom';
 
 const RowActions = ({props}) => {
-    const [hidden, setHidden] = useState(true);
+    const [visibility, setVisibility] = useState(false);
+    const dropdownRef = useRef(null);
     const handleClick = (e) => {
         e.preventDefault();
-        setHidden(!hidden);
+        if (dropdownRef.current !== null && !dropdownRef.current.contains(e.target)) {
+            setVisibility(!visibility);
+        }
     };
+
+    useEffect(() => {
+        if (visibility) {
+            document.addEventListener("click", handleClick);
+        }
+        return () => {
+            document.removeEventListener("click", handleClick);
+        }
+    }, [visibility]);
+
     return (
-        <div className="dropdown">
-            <div className="kebab-icon">
-                <a href="#"  onClick={(e) => handleClick(e)} >
-                    <svg id="menu" width="17" height="17" xmlns="http://www.w3.org/2000/svg">
-                        <circle cx="15%" cy="3" r="2" fill="black"/>
-                        <circle cx="15%" cy="9" r="2" fill="black"/>
-                        <circle cx="15%" cy="15" r="2" fill="black"/>
+        <div className="actions">
+            <div className="kebab-icon" >
+                <a href="#" onClick={(e) => handleClick(e)} >
+                    <svg id="menu" width="18" height="18" xmlns="http://www.w3.org/2000/svg">
+                        <circle cx="50%" cy="3" r="2" fill="black"/>
+                        <circle cx="50%" cy="9" r="2" fill="black"/>
+                        <circle cx="50%" cy="15" r="2" fill="black"/>
                     </svg>
                 </a>
-                    <div className={ hidden ? "dropdown-content-hidden" : "dropdown-content" }>
-                        <ul id="row-dropdown" >
+                    <div className={ visibility ?
+                        "dropdown-content" :
+                        "dropdown-content-hidden"
+                        }
+                        ref={dropdownRef}
+                    >
+                        <ul id="row-dropdown">
                             <li><NavLink to="/inbox/new">Edit</NavLink></li>
                         </ul>
                     </div>
