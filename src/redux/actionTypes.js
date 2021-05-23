70862d27ea4b9f602795bab7300ab4eabcf7ff28
Konstantin Ashaev <techno_nix@mail.ru>
2021-05-23 23:48:27 +0300

    New action type

--- src/redux/actionTypes.js
+++ src/redux/actionTypes.js
@@ -1,4 +1,5 @@
 export const LOGIN = 'LOGIN';
 export const LOGOUT = 'LOGOUT';
 export const INBOX_ADD = 'INBOX_ADD';
+export const INDOX_EDIT = 'INBOX_EDIT';
 export const OUTBOX_ADD = 'OUTBOX_ADD';
