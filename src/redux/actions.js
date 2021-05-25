4b4b768db1a739b469bcda4829ac511b7204c437
Konstantin Ashaev <techno_nix@mail.ru>
2021-05-25 23:10:02 +0300

    New action inbox_edit

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,4 +1,4 @@
-import { LOGIN, LOGOUT, INBOX_ADD, OUTBOX_ADD } from './actionTypes';
+import { LOGIN, LOGOUT, INBOX_ADD, INBOX_EDIT, OUTBOX_ADD } from './actionTypes';
 
 let nextInboxID = 0;
 let nextOutboxID = 0;
@@ -16,6 +16,11 @@ export const logout = () => ({
 export const inbox_add = (content) => ({
     type: INBOX_ADD,
     payload: { id: ++nextInboxID, content }
+});
+
+export const inbox_edit = (inboxId, content) => ({
+    type: INBOX_EDIT,
+    payload: { id: inboxId, content }
 });
 
 export const outbox_add = (content) => ({
