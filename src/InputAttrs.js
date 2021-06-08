adbd1b7dcb9f82cdf11534cdd354aae7c3275c85
Konstantin Ashaev <kostya@gorgaz.ru>
2021-06-08 16:35:10 +0300

    New file. Contains input tag attributes

---
+++ src/InputAttrs.js
@@ -0,0 +1,23 @@
+export const InputInboxAttrs = [
+    {
+        for: "subj"
+        text: "Subject",
+        name: "subj",
+        type: "text",
+    },
+
+    {
+        for: "from",
+        text: "Sender" ,
+        name: "from",
+        type: "text",
+    },
+
+    {
+        for: "note",
+        text: "Note",
+        name: "note",
+        type: "text",
+    }
+
+]; 
