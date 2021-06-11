195e346afe34482fbaa48f25374710cc60d75dc4
Konstantin Ashaev <kostya@gorgaz.ru>
2021-06-11 10:57:56 +0300

    Replace input tags with <InpuField />

--- src/EditInbox.js
+++ src/EditInbox.js
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { inbox_edit } from './redux/actions.js';
 import { connect } from 'react-redux';
+import { InputAttrs as attrs } from './InputAttrs.js';
+import { InputField } from './InputFields.js';
 
 const EditInbox = ({inbox, dispatch}) => {
     const { id } = useParams();
@@ -26,31 +28,19 @@ const EditInbox = ({inbox, dispatch}) => {
 
     return (
         <div className="addRecord">
-          <label for="subj"><b>Subject</b></label>
-          <input
-            type="text"
-            name="subj"
-            value={subj}
-            onChange={(e) => setSubj(e.target.value)}
+        <InputField attrs={attrs.inbox.filter(x => x.name == "subj")[0]}
+                    setter={setSubj}
+                    value={subj}
         />
-
-          <label for="from"><b>Sender</b></label>
-          <input
-            type="text"
-            name="from"
-            value={sender}
-            onChange={(e) => setSender(e.target.value)}
+        <InputField attrs={attrs.inbox.filter(x => x.name == "sender")[0]}
+                    setter={setSender}
+                    value={sender}
         />
-
-          <label for="note"><b>Note</b></label>
-          <input
-            type="text"
-            name="note"
-            value={note}
-            onChange={(e) => setNote(e.target.value)}
+        <InputField attrs={attrs.inbox.filter(x => x.name == "note")[0]}
+                    setter={setNote}
+                    value={note}
         />
-
-          <button type="submit" onClick={(e) => handleEditInbox(e)} >Update</button>
+        <button type="submit" onClick={(e) => handleEditInbox(e)} >Update</button>
         </div>
     );
 }
--- src/NewInbox.js
+++ src/NewInbox.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import { inbox_add } from './redux/actions.js';
 import { connect } from 'react-redux';
+import { InputAttrs as attrs } from './InputAttrs.js';
+import { InputField } from './InputFields.js';
 
 const NewInbox = ({user, dispatch}) => {
     const [subj, setSubj] = useState("");
@@ -27,31 +29,19 @@ const NewInbox = ({user, dispatch}) => {
 
     return (
         <div className="addRecord">
-          <label for="subj"><b>Subject</b></label>
-          <input
-            type="text"
-            name="subj"
-            value={subj}
-            onChange={(e) => setSubj(e.target.value)}
+        <InputField attrs={attrs.inbox.filter(x => x.name == "subj")[0]}
+                    setter={setSubj}
+                    value={subj}
         />
-
-          <label for="from"><b>Sender</b></label>
-          <input
-            type="text"
-            name="from"
-            value={sender}
-            onChange={(e) => setSender(e.target.value)}
+        <InputField attrs={attrs.inbox.filter(x => x.name == "sender")[0]}
+                    setter={setSender}
+                    value={sender}
         />
-
-          <label for="note"><b>Note</b></label>
-          <input
-            type="text"
-            name="note"
-            value={note}
-            onChange={(e) => setNote(e.target.value)}
+        <InputField attrs={attrs.inbox.filter(x => x.name == "note")[0]}
+                    setter={setNote}
+                    value={note}
         />
-
-          <button type="submit" onClick={(e) => handleAddInbox(e)} >Add</button>
+        <button type="submit" onClick={(e) => handleAddInbox(e)} >Add</button>
         </div>
     );
 }
