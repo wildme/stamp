017d3ac12639b73a843ec3b36d1b562651bfaaaf
Konstantin Ashaev <techno_nix@mail.ru>
2021-06-14 22:02:32 +0300

    New object properties

--- src/InputAttrs.js
+++ src/InputAttrs.js
@@ -20,6 +20,28 @@ export const InputAttrs = {
         name: "note",
         type: "text",
     }
+    ],
+    outbox: [
+    {
+        for: "subj",
+        text: "Subject",
+        name: "subj",
+        type: "text",
+    },
+
+    {
+        for: "to",
+        text: "To",
+        name: "to",
+        type: "text",
+    },
+
+    {
+        for: "note",
+        text: "Note",
+        name: "note",
+        type: "text",
+    }
     ]
 
 }; 
