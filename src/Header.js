f25449f3d47317fcebcf2216e8eecfc70f6b04e8
Konstantin Ashaev <kostya@gorgaz.ru>
2021-06-17 16:34:05 +0300

    Change the Header layout

--- src/Header.js
+++ src/Header.js
@@ -5,17 +5,23 @@ import { NavLink } from 'react-router-dom';
 const HeaderLinks = () => {
     return  (
         <div className="navbar">
-            <NavLink to="/inbox">Inbox</NavLink> 
-            <NavLink to="/outbox">Outbox</NavLink>
-                <div className="navbar-right">
-                    <NavLink to="/logout">Logout</NavLink>
-                </div>
+            <div className="navbar-left">
+                <NavLink to="/inbox">Inbox</NavLink> 
+                <NavLink to="/outbox">Outbox</NavLink>
+            </div>
+            <div className="navbar-right">
+                <NavLink to="/logout">Logout</NavLink>
+            </div>
         </div>
     );
 }
 const HeaderBlank = () => {
     return (
         <div className="navbar">
+            <div className="navbar-left">
+            </div>
+            <div className="navbar-right">
+            </div>
         </div>
     );
 }
