fe208399e63edb3b5c8c80f94001b18594145fe3
Konstantin Ashaev <kostya@gorgaz.ru>
2021-07-26 16:20:05 +0300

    Rename object properties

--- src/Row.js
+++ src/Row.js
@@ -3,11 +3,11 @@ import RowActions from './RowActions.js';
 const Row = ({ entry }) => (
   <tr className="inbox-item">
     <td>{entry.id}</td>
-    <td>{entry.subj}</td>
+    <td>{entry.subject}</td>
     <td>{entry.from || entry.to}</td>
     <td>{entry.date}</td>
-    <td>{entry.added}</td>
-    <td>{entry.note}</td>
+    <td>{entry.addedBy}</td>
+    <td>{entry.notes}</td>
     <td>
       <RowActions id={entry.id} />
     </td>
