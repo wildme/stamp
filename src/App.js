75c4bb65ff2b6eeca5775be792119788d6a4921d
Konstantin Ashaev <techno_nix@mail.ru>
2021-08-29 18:06:45 +0300

    Remove Redux code

--- src/App.js
+++ src/App.js
@@ -6,22 +6,16 @@ import Content from './Content.js';
 import { Footer } from './Footer.js';
 import Login from './Login.js';
 
-import { connect } from 'react-redux';
-
-const App = ({ user }) => {
+const App = () => {
   return (
     <Router>
       <Fragment>
         <Header />
-        {user ? <Content user={user.name}/> : <Login />}
+        <Content />
         <Footer />
       </Fragment>
     </Router>
   );
 };
 
-const mapStateToProps = (state) => {
-  const { user } = state;
-  return { user: user };
-};
-export default connect(mapStateToProps)(App);
+export default App;
