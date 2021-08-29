dfc2cbb52ec39a5dd233d0faf29e4e327ff74147
Konstantin Ashaev <techno_nix@mail.ru>
2021-08-29 18:08:17 +0300

    Comment routes. Testing

--- src/Content.js
+++ src/Content.js
@@ -4,20 +4,22 @@ import Main from './Main.js';
 import NewRecord from './NewRecord.js';
 import EditRecord from './EditRecord.js';
 import Logout from './Logout.js';
+import Login from './Login.js';
 import { Route } from 'react-router-dom';
 
-const Content = ({ user }) => {
+const Content = () => {
   return (
     <Fragment>
       <header>
-        <Route exact path="/" render={() => <h1>Hello, {user}</h1>} />
+        <Route exact path="/" render={() => <h1>Hello, Stranger</h1>} />
       </header>
       <main>
-        <Route exact path="/logout" component={Logout} />
+        <Route exact path="/login" component={Login} />
+        {/*<Route exact path="/logout" component={Logout} />
         <Route exact path="/inbox" component={Main} />
         <Route exact path="/outbox" component={Main} />
         <Route exact path="/:box/new" component={NewRecord} />
-        <Route exact path="/:box/edit/:id" component={EditRecord} />
+      <Route exact path="/:box/edit/:id" component={EditRecord} /> */} 
       </main>
     </Fragment>
   );
