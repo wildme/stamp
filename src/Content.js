f44381780a2bcbfd06a525a687aa885a69c2f45f
Konstantin Ashaev <kostya@gorgaz.ru>
2021-09-01 16:22:29 +0300

    Add verifyToken() checking the access token validity. Private routes testing

--- src/Content.js
+++ src/Content.js
@@ -1,13 +1,46 @@
 import './App.css';
+import React, { useState } from 'react';
 import { Fragment } from 'react';
 import Main from './Main.js';
 import NewRecord from './NewRecord.js';
 import EditRecord from './EditRecord.js';
 import Logout from './Logout.js';
 import Login from './Login.js';
-import { Route } from 'react-router-dom';
+import { Route, Redirect } from 'react-router-dom';
 
 const Content = ({ token, user }) => {
+  //const [isValid, setIsValid] = useState(false);
+  //const [expired, setExpired] = useState('');
+
+  const verifyToken = (accessToken) => {
+    fetch('/api/verify/token', {
+      method: 'POST', 
+      body: JSON.stringify({ token }),
+      headers: { 'Content-Type': 'application/json' }
+    })
+    .then(res => res.json())
+    .then(data => {
+      console.log(data.status);
+      if (data.status === 401) return false;
+      if (data.status === 200) return true;
+    })
+    .catch(err => console.log(err))
+  };
+  const PrivateRoute = ({ children, ...rest }) => {
+    const isValid = verifyToken(token);
+    return (
+      <Route
+        {...rest}
+      render={({ location }) => 
+        isValid ? ( children ) : ( <Redirect
+          to={{ pathname: "/login", state: { from: location }}}
+          />
+        )
+      }
+      />
+    );
+  }
+
   return (
     <Fragment>
       <header>
@@ -15,11 +48,26 @@ const Content = ({ token, user }) => {
       </header>
       <main>
         <Route exact path="/login" component={Login} />
+    <PrivateRoute path="/inbox">
+    <Main />
+    </PrivateRoute>
+    <PrivateRoute path="/outbox">
+    <Main />
+    </PrivateRoute>
+    <PrivateRoute path="/:box/new">
+    <NewRecord />
+    </PrivateRoute>
+    <PrivateRoute path="/:box/edit/:id">
+    <EditRecord />
+    </PrivateRoute>
+    {/*
+        <Route exact path="/login" component={Login} />
         <Route exact path="/logout" component={Logout} />
         <Route exact path="/inbox" component={Main} />
         <Route exact path="/outbox" component={Main} />
         <Route exact path="/:box/new" component={NewRecord} />
         <Route exact path="/:box/edit/:id" component={EditRecord} />
+        */}
       </main>
     </Fragment>
   );
