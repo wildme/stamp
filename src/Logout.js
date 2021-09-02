30c3250a5540a5f05488dc9317650340bc1ac7bb
Konstantin Ashaev <kostya@gorgaz.ru>
2021-09-02 15:42:00 +0300

    Remove unused imports

--- src/Logout.js
+++ src/Logout.js
@@ -1,15 +1,10 @@
-import { connect } from 'react-redux';
-import { logout } from './redux/actions.js';
 import { Fragment } from 'react';
-import { Route, Redirect } from 'react-router-dom';
 
-const Logout = ({ dispatch }) => {
-  dispatch(logout());
+const Logout = () => {
   return (
     <Fragment>
-      <Route exact path="/logout" render={() => <Redirect to="/" />} />
     </Fragment>
   );
 };
 
-export default connect()(Logout);
+export default Logout;
