3ee79dc5dbe94e53fd0569a5bfac9b109a37463f
Konstantin Ashaev <techno_nix@mail.ru>
2021-09-04 23:01:43 +0300

    Basic implementation of the logout functionality

--- src/Logout.js
+++ src/Logout.js
@@ -1,5 +1,12 @@
-import { Fragment } from 'react';
+import { Redirect } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
 
-const Logout = () => { fetch('/api/logout'); };
+const Logout = () => {
+  const dispatch = useDispatch();
+
+  fetch('/api/logout');
+  dispatch({ type: 'LOGOUT', payload: { user: 'empty', token: 'empty' }});
+  return <Redirect to='/login' />
+};
 
 export default Logout;
