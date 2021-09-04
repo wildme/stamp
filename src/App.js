958f8794cbeab5e27c8137fc50da84e76cd62116
Konstantin Ashaev <techno_nix@mail.ru>
2021-09-04 22:43:35 +0300

    Delete import definitions and unused code

--- src/App.js
+++ src/App.js
@@ -1,32 +1,12 @@
 import './App.css';
-import React, { useState } from 'react';
-import { useSelector } from 'react-redux';
-import { useDispatch } from 'react-redux';
+import React from 'react';
 import { Fragment } from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import Header from './Header.js';
 import Content from './Content.js';
 import { Footer } from './Footer.js';
-import Login from './Login.js';
 
 const App = () => {
-  //const dispatch = useDispatch();
-  //const token = useSelector((state) => state.token);
-  //const getToken = () => {
-  //  fetch('/api/token', {
-  //    method: 'GET',
-  //    headers: { 'Content-Type': 'application/json' }
-  //  })
-  //  .then(res => res.json())
-  //  .then(data => {
-  //    dispatch({ type: 'TOKEN', payload: { token: data.token }});
-  //  })
-  //  .catch(err => console.log(err))
-  //}
-
-  //if (token === 'empty')  {
-  //  getToken();
-  //}
 
   return (
     <Router>
