09e31a4e0a3188283ee67b7c1e7f5731a3906c8c
Konstantin Ashaev <kostya@gorgaz.ru>
2021-09-22 13:03:36 +0300

    Add tryToRefreshToken() to handle page reloads

--- src/Content.js
+++ src/Content.js
@@ -8,12 +8,29 @@ import NewRecord from './NewRecord.js';
 import EditRecord from './EditRecord.js';
 import Logout from './Logout.js';
 import Login from './Login.js';
-import { Route, Redirect, Switch } from 'react-router-dom';
+import { Route, Redirect,
+  Switch, useHistory, useLocation } from 'react-router-dom';
 
 const Content = () => {
   const user = useSelector((state) => state.user);
   const token = useSelector((state) => state.token);
   const dispatch = useDispatch();
+  const location = useLocation();
+  const history = useHistory();
+
+  const tryToRefreshToken = () => {
+    fetch('/api/refresh/token')
+    .then(res => {
+      if (res.status === 200) return res.json();
+    })
+    .then(data => {
+      dispatch({ type: 'TOKEN', payload:
+          { token: { string: data.token, status: 'valid' }}});
+      dispatch({ type: 'LOGIN', payload: { user: data.user } });
+      history.replace(location.pathname);
+  })
+    .catch(err => console.log(err));
+  };
 
   const verifyToken = (accessToken) => {
     fetch('/api/verify/token', {
@@ -31,7 +48,9 @@ const Content = () => {
   };
 
   const PrivateRoute = ({ component: Component, ...rest }) => {
-    if (token !== 'empty') verifyToken(token.string);
+    token !== 'empty' ? 
+      verifyToken(token.string) :
+      tryToRefreshToken();
     return (
       <Route
         {...rest}
