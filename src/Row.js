81bd0077d53143bb0932c7143acdae6065051d5e
Konstantin Ashaev <kostya@gorgaz.ru>
2022-01-14 16:35:28 +0300

    Provide table rows based on the 'table' variable

--- src/Row.js
+++ src/Row.js
@@ -1,15 +1,29 @@
+import { Fragment } from 'react';
 import RowActions from './RowActions.js';
 
-const Row = ({ entry }) => (
-  <tr className="inbox-item">
-    <td>{entry.id}</td>
-    <td>{entry.subject}</td>
-    <td>{entry.from || entry.to}</td>
-    <td>{new Date(entry.date).toLocaleString('ru-Ru')}</td>
-    <td>{entry.addedBy}</td>
-    <td>{entry.notes}</td>
-    <td><RowActions id={entry.id} /></td>
-  </tr>
-);
+const Row = ({ entry, table }) => {
+  return (
+  <Fragment>
+  { table === 'box' &&
+    <tr className="inbox-item">
+      <td>{entry.id}</td>
+      <td>{entry.subject}</td>
+      <td>{entry.from || entry.to}</td>
+      <td>{new Date(entry.date).toLocaleString('ru-Ru')}</td>
+      <td>{entry.addedBy}</td>
+      <td>{entry.notes}</td>
+      <td><RowActions id={entry.id} /></td>
+    </tr>
+  }
+  { table === 'contacts' &&
+    <tr className="inbox-item">
+      <td>{entry.location}</td>
+      <td>{entry.region}</td>
+      <td>{entry.name}</td>
+    </tr>
+  }
+  </Fragment>
+  )
+};
 
 export default Row;
