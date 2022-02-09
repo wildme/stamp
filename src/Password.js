6f2609ce6e770ad04b0c56fb4d850209e9d76df3
Konstantin Ashaev <kostya@gorgaz.ru>
2022-02-09 16:35:37 +0300

    Add info/error messages

--- src/Password.js
+++ src/Password.js
@@ -1,24 +1,42 @@
 import { useState } from 'react';
 
-const Password = () => {
+const Password = ({user}) => {
   const [oldPass, setOldPass] = useState('');
   const [newPass, setNewPass] = useState('');
   const [confirmPass, setConfirmPass] = useState('');
+  const [infoMsg, setInfoMsg] = useState('');
+  const [errorMsg, setErrorMsg] = useState('');
 
   const handleOldPass = (e) => {
+    setErrorMsg("");
     setOldPass(e.target.value);
   };
 
   const handleNewPass = (e) => {
+    setInfoMsg("");
     setNewPass(e.target.value);
   };
 
   const handleConfirmPass = (e) => {
+    setInfoMsg("");
     setConfirmPass(e.target.value);
   }
   const handlePassUpdate = (e) => {
     e.preventDefault();
-    return;
+    if (newPass !== confirmPass) {
+      setInfoMsg("passwords do not match");
+      return;
+    }
+
+    fetch("/api/user/update/password", {
+      method: 'POST',
+      body: JSON.stringify({user, oldPass, newPass}),
+      headers: {'Content-Type': 'application/json'}
+    })
+      .then(res => {
+        if (res.status === 409) setErrorMsg("wrong password");
+      })
+      .catch((e) => console.error(e))
 };
   return (
     <div className="user-info-grid-container">
@@ -27,7 +45,9 @@ const Password = () => {
       </div>
       <form onSubmit={(e) => handlePassUpdate(e)} autoComplete="off">
         <div className="user-info-input-container">
-          <label htmlFor="old-pass"><b>Old password</b></label>
+          <label htmlFor="old-pass"><b>Old password</b>
+            {errorMsg && <span className="user-info-msg"> {errorMsg}</span>}
+          </label>
           <input
             type="password"
             name="old-pass"
@@ -35,7 +55,9 @@ const Password = () => {
             required
             onChange={(e) => handleOldPass(e)}
           />
-          <label htmlFor="new-pass"><b>New password</b></label>
+          <label htmlFor="new-pass"><b>New password</b>
+            {infoMsg && <span className="user-info-msg"> {infoMsg}</span>}
+          </label>
           <input
             type="password"
             name="new-pass"
@@ -43,7 +65,7 @@ const Password = () => {
             required
             onChange={(e) => handleNewPass(e)}
           />
-          <label htmlFor="confirm-pass"><b>Confirm password</b></label>
+          <label htmlFor="confirm-pass"><b>Confirm new password</b></label>
           <input
             type="password"
             name="confirm-pass"
