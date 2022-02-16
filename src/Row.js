bd17a36cda49a50ca9b5fef3212e872921a4fc67
Konstantin Ashaev <techno_nix@mail.ru>
2022-02-16 21:10:10 +0300

    Contacts: add table row editing

--- src/Row.js
+++ src/Row.js
@@ -1,16 +1,35 @@
 import { Link } from 'react-router-dom';
-import { Fragment, useContext } from 'react';
+import { useState, Fragment, useContext } from 'react';
 import { useSelector } from 'react-redux';
-import { HiPencil } from 'react-icons/hi';
+import { HiPencil, HiCheckCircle, HiXCircle } from 'react-icons/hi';
 import { BoxContext } from './Main.js';
 import { ContactsContext } from './Contacts.js';
 
 const Row = ({ entry }) => {
+  const [editOn, setEditOn] = useState(false);
+  const [newName, setNewName] = useState('');
+  const [newLocation, setNewLocation] = useState('');
+  const [newRegion, setNewRegion] = useState('');
+
   const box = useContext(BoxContext);
   const contacts = useContext(ContactsContext);
   const dateStr = new Date(entry.date).toLocaleString('ru-Ru');
   const user = useSelector((state) => state.user);
   const accessToEdit = user.admin || (user.username === entry.addedBy);
+
+  const handleEdit = (e) => {
+    e.preventDefault();
+    setEditOn(true);
+  };
+  const handleSubmit = (e, id) => {
+    // Add fetch() call here
+    e.preventDefault();
+  };
+
+  const handleCancel = (e) => {
+    e.preventDefault();
+    setEditOn(false);
+  };
 
   return (
   <Fragment>
@@ -29,9 +48,23 @@ const Row = ({ entry }) => {
   }
   { contacts &&
     <tr className="contact-item">
-      <td>{entry.name}</td>
-      <td>{entry.location}</td>
-      <td>{entry.region}</td>
+      <td>{ editOn ? <input type="text" value={entry.name}
+        onChange={(e) => setNewName(e.target.value)} />  : entry.name }
+      </td>
+      <td>{ editOn ? <input type="text" value={entry.location}
+        onChange={(e) => setNewLocation(e.target.value)}/> : entry.location }
+      </td>
+      <td>{ editOn ? <input type="text" value={entry.region}
+        onChange={(e) => setNewRegion(e.target.value)}/> : entry.region}
+      </td>
+      <td>{ !editOn ?
+        <a href="#" onClick={(e) => handleEdit(e)}><HiPencil /></a> :
+        <>
+          <a href="#" onClick={(e) => handleSubmit(e, entry._id)}><HiCheckCircle /></a>
+          <a href="#" onClick={(e) => handleCancel(e)}><HiXCircle /></a>
+        </>
+        }
+      </td>
     </tr>
   }
   </Fragment>
