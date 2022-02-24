8db704250400c76699606689d9dc0b3a0acd09a3
Konstantin Ashaev <techno_nix@mail.ru>
2022-02-24 22:38:41 +0300

    Import a new module file

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import { Header } from './Header.js';
 import Content from './Content.js';
 import { Footer } from './Footer.js';
+import i18n from './i18n/i18n.js';
 
 const App = () => {
 
