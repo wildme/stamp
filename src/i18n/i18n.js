80d9d497b8e211934b553abbef988e6fbf924c78
Konstantin Ashaev <kostya@gorgaz.ru>
2022-02-25 16:19:47 +0300

    Add en/ru translations

--- src/i18n/i18n.js
+++ src/i18n/i18n.js
@@ -1,16 +1,29 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import ru from './ru.json';
+import en from './en.json';
 
 const resources = {
   en: {
     translation: {
-
+      login: {
+        placeholder1: en.login.placeholder1,
+        placeholder2: en.login.placeholder2,
+        button: en.login.button,
+        string: en.login.string,
+        link: en.login.link
+      }
     }
   },
   ru: {
     translation: {
-
+      login: {
+        placeholder1: ru.login.placeholder1,
+        placeholder2: ru.login.placeholder2,
+        button: ru.login.button,
+        string: ru.login.string,
+        link: ru.login.link
+      }
     }
   }
 };
@@ -19,7 +32,7 @@ i18n
   .use(initReactI18next)
   .init({
     resources,
-    lng: "en",
+    lng: 'en',
     interpolation: {
       escapeValue: false
     }
