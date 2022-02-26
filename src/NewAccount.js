367278326b69e1d60758c25410ca43be33150d06
Konstantin Ashaev <techno_nix@mail.ru>
2022-02-26 19:40:03 +0300

    Add an info message and translations. Delete unused hooks

--- src/NewAccount.js
+++ src/NewAccount.js
@@ -1,5 +1,7 @@
 import { useState } from 'react';
 import { useHistory } from 'react-router-dom';
+import { useTranslation } from 'react-i18next';
+import FlashMessage from './FlashMessage.js'
 
 const NewAccount = () => {
   const [username, setUsername] = useState('');
@@ -9,19 +11,18 @@ const NewAccount = () => {
   const [lastname, setLastname] = useState('');
   const [email, setEmail] = useState('');
   const [infoMsg, setInfoMsg] = useState('');
-  const [error, setError] = useState(false);
-
+  const { t } = useTranslation();
   const history = useHistory();
 
   const handleSignup = (e) => {
     e.preventDefault();
-    if (error) setError(false);
+    setInfoMsg('');
 
     if (password !== confirmPassword) {
-      setError(true);
-      setInfoMsg("Passwords don't match");
-      return;
+      setInfoMsg(t('signup.infoMsg1'));
+      return null;
     }
+
     fetch("/api/signup", {
       method: 'POST',
       body: JSON.stringify({ username, password,
@@ -31,18 +32,20 @@ const NewAccount = () => {
     .then(res => {
       if (res.status === 409) return res.json();
       if (res.status === 201) {
-        setInfoMsg('Account created!');
+        setInfoMsg(t('signup.infoMsg5'));
         history.replace('/login');
       }
       if (res.status === 500) {
-        setError(true);
-        setInfoMsg("Couldn't create an account. Try Again.");
+        setInfoMsg(t('signup.infoMsg4'));
       }
     })
     .then(data => {
-      if (data.error) {
-        setError(true);
-        setInfoMsg(data.error);
+      if (data && data.error === 'user exists') {
+        setInfoMsg(t('signup.infoMsg2'));
+      }
+
+      if (data && data.error === 'email exists') {
+        setInfoMsg(t('signup.infoMsg3'));
       }
     })
     .catch((e) => console.error(e))
@@ -50,73 +53,72 @@ const NewAccount = () => {
 
   return (
     <div className="login-grid-container">
+      { infoMsg && <FlashMessage msg={infoMsg} /> }
       <div className="login-form-container">
-    {error &&
-        <div className="login-error-msg">{infoMsg}</div>}
-          <form onSubmit={(e) => handleSignup(e)} autoComplete="off">
-            <div className="login-input-container">
-              <input
-                type="text"
-                placeholder="Username"
-                name="username"
-                value={username}
-                required
-                minLength="3"
-                maxLength="25"
-                onChange={(e) => setUsername(e.target.value)}
-              />
-              <input
-                type="email"
-                placeholder="Email"
-                name="email"
-                value={email}
-                required
-                minLength="6"
-                maxLength="40"
-                onChange={(e) => setEmail(e.target.value)}
-              />
-              <input
-                type="password"
-                placeholder="Password"
-                name="password"
-                value={password}
-                required
-                minLength="8"
-                maxLength="255"
-                onChange={(e) => setPassword(e.target.value)}
-              />
-              <input
-                type="password"
-                placeholder="Confirm password"
-                name="confirm-password"
-                value={confirmPassword}
-                required
-                minLength="8"
-                maxLength="255"
-                onChange={(e) => setConfirmPassword(e.target.value)}
-              />
-              <input
-                type="text"
-                placeholder="Firstname"
-                name="firstname"
-                value={firstname}
-                required
-                maxLength="50"
-                onChange={(e) => setFirstname(e.target.value)}
-              />
-              <input
-                type="text"
-                placeholder="Lastname"
-                name="lastname"
-                value={lastname}
-                maxLength="50"
-                onChange={(e) => setLastname(e.target.value)}
-              />
-            </div>
-            <div className="login-btn-container">
-              <input value="Signup" id="submit" type="submit" />
-            </div>
-          </form>
+        <form onSubmit={(e) => handleSignup(e)} autoComplete="off">
+          <div className="login-input-container">
+            <input
+              type="text"
+              placeholder={t('signup.placeholder1')}
+              name="username"
+              value={username}
+              required
+              minLength="3"
+              maxLength="25"
+              onChange={(e) => setUsername(e.target.value)}
+            />
+            <input
+              type="email"
+              placeholder={t('signup.placeholder2')}
+              name="email"
+              value={email}
+              required
+              minLength="6"
+              maxLength="40"
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <input
+              type="password"
+              placeholder={t('signup.placeholder3')}
+              name="password"
+              value={password}
+              required
+              minLength="8"
+              maxLength="255"
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <input
+              type="password"
+              placeholder={t('signup.placeholder4')}
+              name="confirm-password"
+              value={confirmPassword}
+              required
+              minLength="8"
+              maxLength="255"
+              onChange={(e) => setConfirmPassword(e.target.value)}
+            />
+            <input
+              type="text"
+              placeholder={t('signup.placeholder5')}
+              name="firstname"
+              value={firstname}
+              required
+              maxLength="50"
+              onChange={(e) => setFirstname(e.target.value)}
+            />
+            <input
+              type="text"
+              placeholder={t('signup.placeholder6')}
+              name="lastname"
+              value={lastname}
+              maxLength="50"
+              onChange={(e) => setLastname(e.target.value)}
+            />
+          </div>
+          <div className="login-btn-container">
+            <input value={t('signup.button')} id="submit" type="submit" />
+          </div>
+        </form>
       </div>
     </div>
   );
