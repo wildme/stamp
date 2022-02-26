0596d95a7f03a9a6ff683f55bdda592a44423b94
Konstantin Ashaev <techno_nix@mail.ru>
2022-02-26 19:30:43 +0300

    Add an info message and translations

--- src/Login.js
+++ src/Login.js
@@ -2,11 +2,13 @@ import { useState } from 'react';
 import { useHistory, useLocation, Link } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import { useTranslation } from 'react-i18next';
+import FlashMessage from './FlashMessage.js'
 
 const Login = () => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
-  const [loginFailure, setLoginFailure] = useState(false);
+  const [error, setError] = useState(false);
+  const [infoMsg, setInfoMsg] = useState('');
   const { t } = useTranslation();
 
   const dispatch = useDispatch();
@@ -17,6 +19,9 @@ const Login = () => {
 
   const handleLogin = (e) => {
     e.preventDefault();
+    if (infoMsg) setInfoMsg('');
+    if (error) setError(false);
+
     fetch("/api/login", {
       method: 'POST',
       body: JSON.stringify({ username, password }),
@@ -24,7 +29,13 @@ const Login = () => {
     })
       .then(res => {
         if (res.status === 200) return res.json();
-        if (res.status === 401) setLoginFailure(true);
+        if (res.status === 500) {
+          setError(true);
+          setInfoMsg(t('login.infoMsg2'));
+        }
+        if (res.status === 401) {
+          setInfoMsg(t('login.infoMsg1'));
+        }
       })
       .then(data => {
         if (data) {
@@ -33,9 +44,7 @@ const Login = () => {
             { user: { username: data.user.username,
               admin: data.user.admin, loggedIn: true }}
           });
-            setUsername('');
-            setPassword('');
-            history.replace(from);
+        if (!error) history.replace(from);
         }
       })
       .catch((e) => console.error(e))
@@ -43,35 +52,34 @@ const Login = () => {
 
   return (
     <div className="login-grid-container">
+      { infoMsg && <FlashMessage msg={infoMsg} /> }
       <div className="login-form-container">
-    { loginFailure &&
-        <div className="login-error-msg">Bad username or password</div> }
-          <form onSubmit={(e) => handleLogin(e)}>
-            <div className="login-input-container">
-              <input
+        <form onSubmit={(e) => handleLogin(e)}>
+          <div className="login-input-container">
+            <input
                 type="text"
                 placeholder={t('login.placeholder1')}
                 name="username"
                 value={username}
                 required
                 onChange={(e) => setUsername(e.target.value)}
-              />
-              <input
+            />
+            <input
                 type="password"
                 placeholder={t('login.placeholder2')}
                 name="password"
                 value={password}
                 required
                 onChange={(e) => setPassword(e.target.value)}
-              />
-            </div>
-            <div className="login-btn-container">
-              <input value={t('login.button')} id="submit" type="submit" />
-            </div>
-          </form>
-          <div className="login-signup-msg">{t('login.string')}
-              <Link to="/signup">{t('login.link')}</Link>
+            />
           </div>
+          <div className="login-btn-container">
+              <input value={t('login.button')} id="submit" type="submit" />
+          </div>
+        </form>
+      <div className="login-signup-msg">{t('login.string')}
+        <Link to="/signup">{t('login.link')}</Link>
+      </div>
       </div>
     </div>
   );
