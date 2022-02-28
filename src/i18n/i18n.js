192d74cb57324e9923d8dee4e1c2e9b1ecdd4afc
Konstantin Ashaev <kostya@gorgaz.ru>
2022-02-28 16:12:54 +0300

    Add new i18next resources #2

--- src/i18n/i18n.js
+++ src/i18n/i18n.js
@@ -28,6 +28,55 @@ const resources = {
         infoMsg3: en.signup.infoMsg3,
         infoMsg4: en.signup.infoMsg4,
         infoMsg5: en.signup.infoMsg5
+      },
+      navbar: {
+        inbox: en.navbar.inbox,
+        outbox: en.navbar.outbox,
+        contacts: en.navbar.contacts,
+        profile: en.navbar.profile,
+        logout: en.navbar.logout
+      },
+      contacts: {
+        title: en.contacts.title,
+        infoMsg1: en.contacts.infoMsg1,
+        link: en.contacts.link
+      },
+      userProfile: {
+        subTitle: en.userProfile.subTitle,
+        item1: en.userProfile.item1,
+        item2: en.userProfile.item2,
+        item3: en.userProfile.item3
+      },
+      personalInfo: {
+        title: en.personalInfo.title,
+        label1: en.personalInfo.label1,
+        label2: en.personalInfo.label2,
+        label3: en.personalInfo.label3,
+        button: en.personalInfo.button
+      },
+      email: {
+        title: en.email.title,
+        label1: en.email.label1,
+        button: en.email.button,
+        infoMsg1: en.email.infoMsg1,
+        infoMsg2: en.email.infoMsg2
+      },
+      password: {
+        title: en.password.title,
+        label1: en.password.label1,
+        label2: en.password.label2,
+        label3: en.password.label3,
+        button: en.password.button,
+        infoMsg1: en.password.infoMsg1,
+        infoMsg2: en.password.infoMsg2,
+        infoMsg3: en.password.infoMsg3
+      },
+      main: {
+        titleInbox: en.main.titleInbox,
+        titleOutbox: en.main.titleOutbox,
+        link: en.main.link,
+        infoMsg1: en.main.infoMsg1,
+        infoMsg2: en.main.infoMsg2
       }
     }
   },
@@ -55,6 +104,56 @@ const resources = {
         infoMsg3: ru.signup.infoMsg3,
         infoMsg4: ru.signup.infoMsg4,
         infoMsg5: ru.signup.infoMsg5
+      },
+      navbar: {
+        inbox: ru.navbar.inbox,
+        outbox: ru.navbar.outbox,
+        contacts: ru.navbar.contacts,
+        profile: ru.navbar.profile,
+        logout: ru.navbar.logout
+      },
+      contacts: {
+        title: ru.contacts.title,
+        infoMsg1: ru.contacts.infoMsg1,
+        infoMsg2: ru.contacts.infoMsg2,
+        link: ru.contacts.link
+      },
+      userProfile: {
+        subTitle: ru.userProfile.subTitle,
+        item1: ru.userProfile.item1,
+        item2: ru.userProfile.item2,
+        item3: ru.userProfile.item3
+      },
+      personalInfo: {
+        title: ru.personalInfo.title,
+        label1: ru.personalInfo.label1,
+        label2: ru.personalInfo.label2,
+        label3: ru.personalInfo.label3,
+        button: ru.personalInfo.button
+      },
+      email: {
+        title: ru.email.title,
+        label1: ru.email.label1,
+        button: ru.email.button,
+        infoMsg1: ru.email.infoMsg1,
+        infoMsg2: ru.email.infoMsg2,
+      },
+      password: {
+        title: ru.password.title,
+        label1: ru.password.label1,
+        label2: ru.password.label2,
+        label3: ru.password.label3,
+        button: ru.password.button,
+        infoMsg1: ru.password.infoMsg1,
+        infoMsg2: ru.password.infoMsg2,
+        infoMsg3: ru.password.infoMsg3
+      },
+      main: {
+        titleInbox: ru.main.titleInbox,
+        titleOutbox: ru.main.titleOutbox,
+        link: ru.main.link,
+        infoMsg1: ru.main.infoMsg1,
+        infoMsg2: ru.main.infoMsg2
       }
     }
   }
