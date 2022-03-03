900106316b072874243a3984845898c48a89866c
Konstantin Ashaev <techno_nix@mail.ru>
2022-03-03 22:05:09 +0300

    Add useEffect, hooks. Accept 't' property

--- src/Email.js
+++ src/Email.js
@@ -1,27 +1,38 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
 
-const Email = ({ user, email, setter }) => {
+const Email = ({ user, t }) => {
+  const [email, setEmail] = useState('');
   const [infoMsg, setInfoMsg] = useState('');
-  const { t } = useTranslation();
 
-  const handleEmailChange = (e) => {
-    setter(e.target.value);
-};
-
-const handleEmailUpdate = (e) => {
-  e.preventDefault();
-  fetch("/api/user/update/email", {
-    method: 'POST',
-    body: JSON.stringify({user, email}),
-    headers: {'Content-Type': 'application/json'}
-  })
-    .then(res => {
-      if (res.status === 500) setInfoMsg(t('email.label1'));
-      if (res.status === 409) setInfoMsg(t('email.label2'));
+  const handleEmailUpdate = (e) => {
+    e.preventDefault();
+    fetch("/api/user/update/email", {
+      method: 'POST',
+      body: JSON.stringify({user, email}),
+      headers: {'Content-Type': 'application/json'}
     })
-    .catch((e) => console.error(e))
-};
+      .then(res => {
+        if (res.status === 500) setInfoMsg(t('email.infoMsg1'));
+        if (res.status === 409) setInfoMsg(t('email.infoMsg2'));
+      })
+      .catch((e) => console.error(e))
+  };
+  useEffect(() => {
+    const abortController = new AbortController();
+    const { signal } = abortController;
+
+    fetch(`/api/user/${user}`, { signal })
+      .then(res => {
+        if (res.status === 200) return res.json();
+      })
+      .then(data => {
+        setEmail(data.email);
+      })
+      .catch((e) => console.error(e))
+
+    return () => { abortController.abort(); };
+  }, []);
 
   return (
     <div className="user-info-grid-container">
@@ -34,10 +45,13 @@ const handleEmailUpdate = (e) => {
           type="email"
           name="email"
           value={email}
-          onChange={(e) => handleEmailChange(e)}
+          required
+          onChange={(e) => setEmail(e.target.value)}
         />
       </div>
-      {infoMsg && <div className="user-info-msg">{infoMsg}</div>}
+      { infoMsg &&
+      <div className="user-info-msg">{infoMsg}</div>
+      }
       <div className="user-info-update-btn-container">
         <button
           type="submit"
