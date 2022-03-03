ad6f5fb5e81d7a88a2dd85b134f2dd40417df4e4
Konstantin Ashaev <techno_nix@mail.ru>
2022-03-03 22:07:07 +0300

    Add useEffect, hooks. Accept 't' property

--- src/PersonalInfo.js
+++ src/PersonalInfo.js
@@ -1,18 +1,14 @@
-import { useState } from 'react';
-import { useTranslation } from 'react-i18next';
+import { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 
-const PersonalInfo = ({ user, firstname, lastname, setterF, setterL }) => {
+const PersonalInfo = ({ user, t }) => {
+  const [firstname, setFirstname] = useState('');
+  const [lastname, setLastname] = useState('');
   const [error, setError] = useState(false);
   const [infoMsg, setInfoMsg] = useState('');
-  const { t } = useTranslation();
 
-  const handleFirstnameChange = (e) => {
-    setterF(e.target.value);
-  };
-
-  const handleLastnameChange = (e) => {
-    setterL(e.target.value);
-  };
+  const dispatch = useDispatch();
+  const state = useSelector((state) => state.user);
 
   const handleInfoUpdate = (e) => {
     e.preventDefault();
@@ -22,13 +18,34 @@ const PersonalInfo = ({ user, firstname, lastname, setterF, setterL }) => {
       headers: {'Content-Type': 'application/json'}
     })
       .then(res => {
+        if (res.status === 200) {
+          dispatch({ type: 'LOGIN', payload:
+            { user: {...state,  fullname: [firstname, lastname].join(' ') }}});
+        }
         if (res.status === 500) {
           setError(true);
           setInfoMsg(t('personalInfo.infoMsg'));
         }
       })
       .catch((e) => console.error(e))
-};
+  };
+
+  useEffect(() => {
+    const abortController = new AbortController();
+    const { signal } = abortController;
+
+    fetch(`/api/user/${user}`, { signal })
+      .then(res => {
+        if (res.status === 200) return res.json();
+      })
+      .then(data => {
+        setFirstname(data.firstname);
+        setLastname(data.lastname);
+      })
+      .catch((e) => console.error(e))
+
+    return () => { abortController.abort(); };
+  }, []);
 
   return (
     <div className="user-info-grid-container">
@@ -41,14 +58,15 @@ const PersonalInfo = ({ user, firstname, lastname, setterF, setterL }) => {
           type="text"
           name="firstname"
           value={firstname}
-          onChange={(e) => handleFirstnameChange(e)}
+          required
+          onChange={(e) => setFirstname(e.target.value)}
         />
         <label htmlFor="lastname"><b>{ t('personalInfo.label2') }</b></label>
         <input
           type="text"
           name="lastname"
           value={lastname}
-          onChange={(e) => handleLastnameChange(e)}
+          onChange={(e) => setLastname(e.target.value)}
         />
       </div>
       <div className="user-info-update-btn-container">
