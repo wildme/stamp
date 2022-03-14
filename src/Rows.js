cf32493132672d35808ef33dea7a6befb56542ca
Konstantin Ashaev <konstantin.ashaev@gmail.com>
2022-03-14 20:50:14 +0300

    Choose between tables to render using  the 'kind' property

--- src/Rows.js
+++ src/Rows.js
@@ -1,7 +1,18 @@
-import Row from './Row.js';
+import RowBox from './RowBox.js';
+import RowContacts from './RowContacts.js';
 
-const Rows = ({ rows }) => {
-  return rows.map((row, index) => { return <Row entry={row} key={index} />; });
+const Rows = ({ rows, kind }) => {
+  if (kind === 'box') {
+    return rows.map((row, index) => {
+      return <RowBox entry={row} key={index} />;
+    });
+  }
+
+  if (kind === 'contacts') {
+    return rows.map((row, index) => {
+      return <RowContacts entry={row} key={index} />;
+    });
+  }
 };
 
 export default Rows;
