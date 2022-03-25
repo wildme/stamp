88c8d5d37de8e10eca6c809e8b453b9483d965a6
Konstantin Ashaev <kostya@gorgaz.ru>
2022-03-25 15:19:30 +0300

    Add useEffect() to reset the 'hidden' attribute

--- src/FlashMessage.js
+++ src/FlashMessage.js
@@ -1,11 +1,13 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { HiX } from 'react-icons/hi';
 
-const FlashMessage = ({msg}) => {
+const FlashMessage = (props) => {
   const [hidden, setHidden] = useState(false);
 
+  useEffect(() => {setHidden(false)}, [props.id]);
+
   return (
-    <div className="flash-msg-container" hidden={hidden}>{msg}
+    <div className="flash-msg-container" hidden={hidden}>{props.msg}
       <span className="flash-msg-btn">
         <button onClick={() => setHidden(true)}><HiX /></button>
       </span>
