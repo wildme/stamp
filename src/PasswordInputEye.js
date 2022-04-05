9f684549a4430c4194394803216429a7510959a2
Konstantin Ashaev <kostya@gorgaz.ru>
2022-04-05 10:27:36 +0300

    New file

---
+++ src/PasswordInputEye.js
@@ -0,0 +1,35 @@
+import { useState } from 'react';
+import { HiEyeOff, HiEye } from 'react-icons/hi';
+
+const PasswordInputEye = (props) => {
+  const [showPass, setShowPass] = useState(false);
+
+  const handleShowPass = (e) => {
+    e.preventDefault();
+    setShowPass(!showPass);
+  };
+
+  return (
+    <div className="user-info-pass-container">
+      <input
+        type={showPass ? "text" : "password"}
+        placeholder={props.place ? props.place : ""}
+        id="pass"
+        name={props.name ? props.name : "pass"}
+        value={props.pass}
+        required
+        minLength="8"
+        maxLength="255"
+        onChange={(e) => props.setter(e.target.value)}
+      />
+      <button
+        id="pass"
+        type="button"
+        onClick={(e) => handleShowPass(e)}>
+        { showPass ? <HiEye /> : <HiEyeOff /> }
+      </button>
+    </div>
+  );
+};
+
+export default PasswordInputEye;
