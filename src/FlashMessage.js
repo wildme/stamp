6f696325b1481f38c1b2ef3c193692d9e095ab4b
Konstantin Ashaev <kostya@gorgaz.ru>
2022-04-06 11:03:10 +0300

    Add inline styles. Add the 'type' property

--- src/FlashMessage.js
+++ src/FlashMessage.js
@@ -3,11 +3,26 @@ import { HiX } from 'react-icons/hi';
 
 const FlashMessage = (props) => {
   const [hidden, setHidden] = useState(false);
+  const styles = {};
 
+  switch (props.type) {
+    case 'success':
+      styles["backgroundColor"] = "#2eb82e";
+      break;
+    case 'info':
+      styles["backgroundColor"] = "#99ccffi";
+      break;
+    default:
+      styles["backgroundColor"] = "#ff4d4d";
+      break;
+  }
   useEffect(() => {setHidden(false)}, [props.id]);
 
   return (
-    <div className="flash-msg-container" hidden={hidden}>{props.msg}
+    <div
+      className="flash-msg-container"
+      hidden={hidden}
+      style={{...styles}}>{props.msg}
       <span className="flash-msg-btn">
         <button onClick={() => setHidden(true)}><HiX /></button>
       </span>
