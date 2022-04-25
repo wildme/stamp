7b05d258f61c69e6c9a05c7c6883208ebbc0a440
Konstantin Ashaev <kostya@gorgaz.ru>
2022-04-25 15:47:02 +0300

    Add translations. Use first name in the greeting message

--- src/Hello.js
+++ src/Hello.js
@@ -1,8 +1,12 @@
 import { useSelector } from 'react-redux';
+import { useTranslation } from 'react-i18next';
 
 const Hello = () => {
-  const user = useSelector((state) => state.user);
-  return <h1>Hello, { user.username }</h1>
+  const fullname = useSelector((state) => state.info.fullname);
+  const [firstname] = fullname.split(' ');
+  const { t } = useTranslation();
+
+  return <h1>{t('homepage.string1')}, {firstname}!</h1>
 }
 
 export default Hello;
