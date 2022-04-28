54a27520de2c5afeea5d08e9d0c381b8b20345a1
Konstantin Ashaev <kostya@gorgaz.ru>
2022-04-28 12:27:34 +0300

    Move the routing logic, sidemenu, title to separate files

--- src/UserProfile.js
+++ src/UserProfile.js
@@ -1,75 +1,30 @@
 import { useState } from 'react';
-import { Link, Switch, Route, useRouteMatch } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import { useTranslation } from 'react-i18next';
-import PersonalInfo from './PersonalInfo.js';
-import Email from './Email.js';
-import Password from './Password.js';
+import UserProfileMenu from './UserProfileMenu.js';
+import UserProfileTitle from './UserProfileTitle.js';
+import UserProfileContent from './UserProfileContent.js';
 import FlashMessage from './FlashMessage.js';
 
 const UserProfile = () => {
-  const { path, url } = useRouteMatch();
   const [infoMsg, setInfoMsg] = useState({str: '', id: 0, type: 'error' });
   const username = useSelector((state) => state.user.username);
   const fullname = useSelector((state) => state.info.fullname);
-  const [name1, name2] = fullname.split(' ');
   const { t } = useTranslation();
 
   return (
     <div className="user-profile-grid-container">
-    { infoMsg.str &&
-        <FlashMessage msg={infoMsg.str} id={infoMsg.id } type={infoMsg.type}/> }
+    {infoMsg.str &&
+       <FlashMessage msg={infoMsg.str} id={infoMsg.id } type={infoMsg.type}/>}
       <div className="user-profile-container">
-        <div className="user-profile-title">
-          <h2>{ fullname }</h2>
-          <p>{ t('userProfile.subTitle') }</p>
-        </div>
-        <div className="user-profile-navbar">
-          <ul>
-            <li>
-              <Link to={`${path}`} id="active">{ t('userProfile.item1') }</Link>
-            </li>
-            <li>
-              <Link to={`${path}/e-mail`}>{ t('userProfile.item2') }</Link>
-            </li>
-            <li>
-              <Link to={`${path}/password`}>{ t('userProfile.item3') }</Link>
-            </li>
-          </ul>
-        </div>
-        <div className="user-profile-info">
-          <Switch>
-            <Route path={`${url}/e-mail`}
-              render={() =>
-                <Email
-                  user={username}
-                  t={t}
-                  setter={setInfoMsg}
-                />
-              }
-            />
-            <Route path={`${url}/password`}
-              render={() =>
-                  <Password
-                    user={username}
-                    t={t}
-                    setter={setInfoMsg}
-                  />
-              }
-            />
-            <Route path="*"
-              render={() =>
-                  <PersonalInfo
-                    user={username}
-                    name1={name1}
-                    name2={name2}
-                    t={t}
-                    setter={setInfoMsg}
-                  />
-              }
-            />
-          </Switch>
-        </div>
+        <UserProfileTitle t={t} fullname={fullname}/>
+        <UserProfileMenu t={t} />
+        <UserProfileContent
+          t={t}
+          username={username}
+          fullname={fullname}
+          setInfoMsg={setInfoMsg}
+        />
       </div>
     </div>
   );
