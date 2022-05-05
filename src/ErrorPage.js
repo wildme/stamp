02f42148e6a32061487cabec48ea362dc024d53d
Konstantin Ashaev <kostya@gorgaz.ru>
2022-05-05 14:15:32 +0300

    Add props to enable conditional rendering

--- src/ErrorPage.js
+++ src/ErrorPage.js
@@ -1,21 +1,27 @@
 import { Link } from 'react-router-dom';
 
-const PageNotFound = () => {
+const ErrorPage = (props) => {
+  const code = props.code;
+
   return (
-    <div className="error404-grid-container">
-      <div className="error404-main-title">
-        <h2>Oops! Page not found.</h2>
+    <div className="error-page-grid-container">
+      <div className="error-page-reason-title">
+        {code === 404 && <h2>Oops! Page not found</h2>}
+        {code === 401 && <h2>Not permitted</h2>}
       </div>
-      <div className="error404-bg-code">
-        <h1>404</h1>
+      <div className="error-page-code-title">
+        <h1>{code}</h1>
       </div>
-      <div className="error404-description">
-        <h2>The page you are looking for is not available.</h2>
+      <div className="error-page-description">
+        {code === 404 &&
+            <h2>The page you are looking for is not available.</h2>}
+        {code === 401 &&
+            <h2>You aren't allow to visit this page.</h2>}
       </div>
-      <div className="error404-home-link">
+      <div className="error-page-home-link">
         <Link to={"/"}>Go back home</Link>
       </div>
     </div>
   )
 };
-export default PageNotFound;
+export default ErrorPage;
