7dfe6dc1df8969ef5067181a389ebc9c4226093a
Konstantin Ashaev <techno_nix@mail.ru>
2022-05-13 23:04:26 +0300

    Remove useHistory(). Move API call to separate functions

--- src/EditRecord.js
+++ src/EditRecord.js
@@ -1,5 +1,5 @@
-import { useState, useEffect } from 'react';
-import { useParams, useHistory } from 'react-router-dom';
+import { useState, useEffect, useRef } from 'react';
+import { useParams } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import { useTranslation } from 'react-i18next';
 import { InputAttrs as attrs } from './InputAttrs.js';
@@ -19,9 +19,71 @@ const EditRecord = () => {
   const [newFile, setNewFile] = useState(null);
   const [noData, setNoData] = useState(false);
   const [infoMsg, setInfoMsg] = useState({str: '', id: 0});
-  const history = useHistory();
   const { t } = useTranslation();
   const user = useSelector((state) => state.user.username);
+  const ref = useRef(null);
+  let uploadedFile = null;
+
+  function uploadFile() {
+    const formData = new FormData();
+    formData.append('file', newFile);
+
+    return fetch(`/api/${box}/upload`, {
+      method: 'POST',
+      body: formData,
+      })
+      .then(res => {
+        if (res.status === 200) {
+          return res.json();
+        }
+        if (res.status === 413) {
+          setInfoMsg({str: t('editRecord.infoMsg5'), id: Math.random()});
+          setNewFile(null);
+          ref.current.value = '';
+          return 'error';
+         }
+        if (res.status === 500) {
+          setInfoMsg({str: t('editRecord.infoMsg3'), id: Math.random()});
+          return 'error';
+         }
+       })
+      .then(data => data)
+      .catch((e) => console.error(e))
+  }
+
+  function deleteFile() {
+    const fileId = file._id;
+
+    fetch(`/api/attachment/delete/${fileId}`)
+      .then(res => {
+        if (res.status === 500) {
+          setInfoMsg({str: t('editRecord.infoMsg2'), id: Math.random()});
+        }
+      })
+      .catch((e) => console.error(e))
+    return;
+  }
+
+  function saveRecord() {
+    fetch(`/api/${box}/update/${id}`, {
+      method: 'POST',
+      body: JSON.stringify({subject, fromTo, replyTo, note, uploadedFile}),
+      headers: {'Content-Type': 'application/json'}
+      })
+      .then(res => {
+        if (res.status === 200) {
+          ref.current.value = '';
+          setInfoMsg(
+            {str: t('editRecord.infoMsg6'), id: Math.random(), type: 'success'}
+          );
+        }
+        if (res.status === 500) {
+          setInfoMsg({str: t('editRecord.infoMsg1'), id: Math.random()});
+        }
+      })
+      .catch((e) => console.error(e))
+    return;
+  }
 
   const handleDownload = (e) => {
     e.preventDefault();
@@ -38,64 +100,17 @@ const EditRecord = () => {
       .catch((e) => console.error(e))
   };
 
-  const handleEditRecord = (e) => {
-    e.preventDefault();
-    fetch(`/api/${box}/update/${id}`, {
-      method: 'POST',
-      body: JSON.stringify({ subject, fromTo, replyTo, note }),
-      headers: { 'Content-Type': 'application/json' }
-      })
-      .then(res => {
-        if ((res.status === 200) && !newFile && !delFile) {
-          history.push(`/${box}`);
-        }
-        if (res.status === 500) {
-          setInfoMsg({str: t('editRecord.infoMsg1'), id: Math.random()});
-        }
-      })
-      .catch((e) => console.error(e))
-
-    if (delFile || (newFile && file)) {
-      const fileId = file._id;
-      fetch(`/api/attachment/delete/${fileId}`)
-        .then(res => {
-          if ((res.status === 200) && !newFile) {
-            history.push(`/${box}`);
-          }
-          if (res.status === 500) {
-            setInfoMsg({str: t('editRecord.infoMsg2'), id: Math.random()});
-          }
-        })
-        .catch((e) => console.error(e))
-    }
-
-    if (newFile) {
-      const formData = new FormData();
-      formData.append('file', newFile);
-      fetch(`/api/${box}/upload/${id}`, {
-        method: 'POST',
-        body: formData,
-       })
-       .then(res => {
-         if (res.status === 200) {
-            history.push(`/${box}`);
-         }
-         if (res.status === 413) {
-           setInfoMsg({str: t('editRecord.infoMsg5'), id: Math.random()});
-         }
-         if (res.status === 500) {
-           setInfoMsg({str: t('editRecord.infoMsg3'), id: Math.random()});
-         }
-       })
-       .catch((e) => console.error(e))
-    }
+  const handleEditRecord = async () => {
+    if (newFile) uploadedFile = await uploadFile();
+    if (delFile || (newFile && file)) deleteFile();
+    if (uploadedFile !== 'error') saveRecord();
   };
 
   useEffect(() => {
     const abortController = new AbortController();
     const signal = abortController.signal;
 
-    fetch(`/api/${box}/${id}`, { signal })
+    fetch(`/api/${box}/${id}`, {signal})
       .then(res =>  {
         if (res.status === 200) return res.json();
         if (res.status === 204) setNoData(true);
@@ -116,7 +131,7 @@ const EditRecord = () => {
         })
         .catch((e) => console.error(e))
 
-    fetch(`/api/attachment/${box}/${id}`, { signal })
+    fetch(`/api/attachment/${box}/${id}`, {signal})
       .then(res => {
         if (res.status === 200) return res.json();
         if (res.status === 204) setFile(null);
@@ -127,14 +142,16 @@ const EditRecord = () => {
       .then(data => setFile(data))
       .catch((e) => console.error(e))
 
-    return () => { abortController.abort(); };
+    return () => {abortController.abort();};
   }, [box, id, t, user]);
 
     if (noData) return <ErrorPage code={404} />;
     if (!permitted) return <ErrorPage code={401} />;
+
     return (
       <div className="edit-record-grid">
-        {infoMsg.str && <FlashMessage msg={infoMsg.str} id={infoMsg.id}/>}
+        {infoMsg.str &&
+          <FlashMessage msg={infoMsg.str} id={infoMsg.id} type={infoMsg.type} />}
         <div className="edit-record">
             <InputField
               attrs={attrs[`${box}`].filter((x) => x.name === 'subj')[0]}
@@ -162,11 +179,13 @@ const EditRecord = () => {
               value={note}
               className="edit-record__input"
             />
-           <input
+            <input
              className="edit-record__upload"
              type="file"
+             name="file"
+             ref={ref}
              onChange={(e) => setNewFile(e.target.files[0])}
-          />
+            />
             {file &&
             <div>
               <a
@@ -183,12 +202,12 @@ const EditRecord = () => {
               />
               <label htmlFor="del-file">{t('editRecord.label1')}</label>
             </div>
-            }
+           }
             <button
               className="edit-record__submit"
               type="submit"
               disabled={!subject || !fromTo}
-              onClick={(e) => handleEditRecord(e)}>
+              onClick={() => handleEditRecord()}>
               {t('editRecord.button')}
             </button>
         </div>
