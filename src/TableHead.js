5b6284e1c507f04d2ddf830d501ca474bc77446c
Konstantin Ashaev <techno_nix@mail.ru>
2022-05-18 16:36:11 +0300

    Wrap returned table header in a <thead> tag

--- src/TableHead.js
+++ src/TableHead.js
@@ -1,23 +1,36 @@
 import { headerColumns } from './headerColumns.js';
 import SortIcon from './sortIcon.js';
 
-const TableHead = ({ table, handleClick, sortOrder, column, t }) =>
-    headerColumns[`${table}`].map((item) => {
-    const { id = '', label = '', sortable } = item;
-    const currentItem = column === id;
-    const direction = currentItem ? sortOrder : '';
+const TableHead = (props) => {
+  const table = props.table;
+  const handleClick = props.handleClick;
+  const sortOrder = props.sortOrder;
+  const column = props.column;
+  const t = props.t;
 
-    return (
-      <th key={id}>{sortable ?
-        <button
-          className="page-table__button"
-          type="button"
-          onClick={() => handleClick(id)}
-        >
-          {t(label)} <SortIcon direction={direction} />
-        </button> : t(label)}
-      </th>
-    )
-   }) 
+  return (
+    <thead className="page-table__thead">
+      <tr className="page-table__tr">
+        {headerColumns[`${table}`].map((item) => {
+          const { id = '', label = '', sortable } = item;
+          const currentItem = column === id;
+          const direction = currentItem ? sortOrder : '';
+
+          return (
+            <th key={id}>{sortable ?
+              <button
+                className="page-table__button"
+                type="button"
+                onClick={() => handleClick(id)}
+              >
+                {t(label)} <SortIcon direction={direction} />
+              </button> : t(label)}
+            </th>
+          )
+        })}
+      </tr>
+    </thead>
+  )
+};
 
 export default TableHead;
