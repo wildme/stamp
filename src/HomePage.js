a418cc51b2e1ca8bf9c273c0e5fdba214a15b665
Konstantin Ashaev <kostya@gorgaz.ru>
2022-06-01 10:45:22 +0300

    Rename component. Add 'ActionCard' components

--- src/HomePage.js
+++ src/HomePage.js
@@ -1,12 +1,26 @@
 import { useSelector } from 'react-redux';
 import { useTranslation } from 'react-i18next';
+import ActionCard from './ActionCard.js';
 
-const Hello = () => {
+const HomePage = () => {
   const fullname = useSelector((state) => state.info.fullname);
   const [firstname] = fullname.split(' ');
   const { t } = useTranslation();
 
-  return <h1>{t('homepage.string1')}, {firstname}!</h1>
+  return (
+    <div className="home-page-grid">
+      <div className="home-page-title">
+        <h1 className="home-page-title__h1">
+          {t('homepage.string1')}, {firstname}!
+        </h1>
+      </div>
+      <div className="home-page-actions-grid">
+        <ActionCard path="/inbox/new" title={t('homepage.action1')} />
+        <ActionCard path="/outbox/new" title={t('homepage.action2')} />
+        <ActionCard path="/contacts/new" title={t('homepage.action3')} />
+      </div>
+    </div>
+  );
 }
 
-export default Hello;
+export default HomePage;
