aebca0cd3875c01c30ea596b8c1efbc629616247
Konstantin Ashaev <kostya@gorgaz.ru>
2022-07-13 15:46:51 +0300

    Add route to 'Settings'. Add a variable.

--- src/UserProfileContent.js
+++ src/UserProfileContent.js
@@ -2,12 +2,14 @@ import { Switch, Route } from 'react-router-dom';
 import UserInfo from './UserInfo.js';
 import UserEmail from './UserEmail.js';
 import UserPassword from './UserPassword.js';
+import UserSettings from './UserSettings.js';
 
 const UserProfileContent = (props) => {
   const t = props.t;
   const username = props.username;
   const fullname = props.fullname;
   const setInfoMsg = props.setInfoMsg;
+  const settings = props.settings;
   const [name1, name2] = fullname.split(' ');
 
   return (
@@ -26,6 +28,16 @@ const UserProfileContent = (props) => {
           render={() =>
               <UserPassword
                 user={username}
+                t={t}
+                setter={setInfoMsg}
+              />
+          }
+        />
+        <Route path="/my-profile/settings"
+          render={() =>
+              <UserSettings
+                user={username}
+                settings={settings}
                 t={t}
                 setter={setInfoMsg}
               />
