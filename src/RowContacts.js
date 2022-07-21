0cb76c2de6b77c13504509f5c4514ec3bc15f554
Konstantin Ashaev <kostya@gorgaz.ru>
2022-07-21 15:30:33 +0300

    Get rid of the hidden attribute inside <tr>. Change formatting

--- src/RowContacts.js
+++ src/RowContacts.js
@@ -8,7 +8,7 @@ const RowContacts = ({ entry }) => {
   const [name, setName] = useState(entry.name);
   const [location, setLocation] = useState(entry.location);
   const [region, setRegion] = useState(entry.region);
-  const [hidden, setHidden] = useState(false);
+  const [deleted, setDeleted] = useState(false);
   const { t } = useTranslation();
 
   const setter = useContext(ContactsContext);
@@ -19,7 +19,7 @@ const RowContacts = ({ entry }) => {
     if (window.confirm(t('editContact.confirm'))) {
       fetch(`/api/contact/delete/${id}`)
         .then(res => {
-          if (res.ok) setHidden(true);
+          if (res.ok) setDeleted(true);
           if (res.status === 500) {
             setter({str: t('editContact.infoMsg2'), id: Math.random()});
           }
@@ -51,51 +51,52 @@ const RowContacts = ({ entry }) => {
     setRegion(entry.region);
   };
 
-  return (
+  return deleted ? null : (
     <Fragment>
-      <tr className="page-table__tr" hidden={hidden}>
+      <tr className="page-table__tr">
         <td className="page-table__td">
           {editOn ?
-            <input type="text" value={name}
-              onChange={(e) => setName(e.target.value)}/> :
-              name
-          }
+            <input
+              type="text"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            /> : name}
         </td>
         <td className="page-table__td">
           {editOn ?
-            <input type="text" value={location}
-              onChange={(e) => setLocation(e.target.value)}/> :
-              location}
+            <input
+              type="text"
+              value={location}
+              onChange={(e) => setLocation(e.target.value)}
+            /> : location}
         </td>
         <td className="page-table__td">
           {editOn ?
-            <input type="text" value={region}
-              onChange={(e) => setRegion(e.target.value)}/> :
-              region}
+            <input
+              type="text"
+              value={region}
+              onChange={(e) => setRegion(e.target.value)}
+            /> : region}
         </td>
         <td className="page-table__td">{!editOn ?
             <>
               <button
                 className="page-table__button"
-                onClick={() => handleEdit()}>
-                <HiPencil />
+                onClick={() => handleEdit()}><HiPencil />
               </button>
               <button
                 className="page-table__button"
-                onClick={() => handleDelete(entry._id)}>
-                <HiTrash />
+                onClick={() => handleDelete(entry._id)}><HiTrash />
               </button>
             </> :
             <>
               <button
                 className="page-table__button"
-                onClick={() => handleSubmit(entry._id)}>
-                <HiCheckCircle />
+                onClick={() => handleSubmit(entry._id)}><HiCheckCircle />
               </button>
               <button
                 className="page-table__button"
-                onClick={() => handleCancel()}>
-                <HiXCircle />
+                onClick={() => handleCancel()}><HiXCircle />
               </button>
             </>
           }
