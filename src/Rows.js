1b362c5a5e72bc28038ee17762e75a08167442cd
Konstantin Ashaev <kostya@gorgaz.ru>
2022-08-05 12:42:57 +0300

    Add a handler for download links. Pass it to RowBox

--- src/Rows.js
+++ src/Rows.js
@@ -1,12 +1,27 @@
 import RowBox from './RowBox.js';
 import RowContacts from './RowContacts.js';
 
+function handleDownload(e, hash, name) {
+  e.preventDefault();
+  fetch(`/api/download/${hash}`)
+    .then(res => res.blob())
+    .then(blob => {
+      const objectURL = URL.createObjectURL(blob);
+      const a = document.createElement('a');
+      a.href = objectURL;
+      a.download = name;
+      a.click();
+      URL.revokeObjectURL(a.href);
+    })
+    .catch((e) => console.error(e))
+}
+
 const Rows = ({ rows, kind }) => {
   if (kind === 'box') {
     return (
       <tbody>
         {rows.map((row, index) => {
-          return <RowBox entry={row} key={index} />;
+          return <RowBox entry={row} key={index} getFile={handleDownload} />;
         })}
       </tbody>
     );
