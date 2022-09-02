3c58a7a268acd80384d33cf84e64adeeb095684c
Konstantin Ashaev <kostya@gorgaz.ru>
2022-09-02 11:26:11 +0300

    Add the Autorization header. dispatch actions in case of a new token or 401 #2

--- src/Contacts.js
+++ src/Contacts.js
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import { useTranslation } from 'react-i18next';
 import TableContacts from './TableContacts.js';
 import FlashMessage from './FlashMessage.js';
@@ -7,16 +8,22 @@ const Contacts = () => {
   const [tableData, setTableData] = useState(null);
   const [noData, setNoData] = useState(false);
   const [infoMsg, setInfoMsg] = useState({str: '', id: 0});
+  const token = useSelector((state) => state.token.string);
+  const dispatch = useDispatch();
   const { t } = useTranslation();
 
   useEffect(() => {
     const abortController = new AbortController();
     const signal = abortController.signal;
+    const url = "/api/contacts";
 
-    fetch("/api/contacts", {signal})
+    fetch(url, { headers: { 'Authorization': `Bearer ${token}` }, signal })
       .then(res => {
         if (res.status === 200) {
           return res.json();
+        }
+        if (res.status === 401) {
+          dispatch({type: 'LOGIN', payload: { user: { loggedIn: false } }});
         }
         if (res.status === 204) {
           setNoData(true);
@@ -25,11 +32,16 @@ const Contacts = () => {
           setInfoMsg({str: t('contacts.infoMsg1'), id: Math.random()});
         }
       })
-      .then(data => setTableData(data))
+      .then(data => {
+        if (data.token) {
+          dispatch({type: 'TOKEN', payload: { token: { string: data.token } }});
+        }
+        setTableData(data.contacts)
+      })
       .catch((e) => console.error(e))
 
     return () => { abortController.abort(); };
-  }, [t])
+  }, [t, dispatch, token])
 
   return (
     <div className="page-content-grid">
