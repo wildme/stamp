683e4b808ab00afeb793a725710c77a643cbf836
Konstantin Ashaev <kostya@gorgaz.ru>
2022-09-02 16:27:24 +0300

    Show '403' when the access denied

--- src/ErrorPage.js
+++ src/ErrorPage.js
@@ -9,14 +9,14 @@ const ErrorPage = (props) => {
     <div className="error-page-grid">
       <div className="error-page__top-title">
         {code === 404 && <h2>{t('errorPage.title1')}</h2>}
-        {code === 401 && <h2>{t('errorPage.title2')}</h2>}
+        {code === 403 && <h2>{t('errorPage.title2')}</h2>}
       </div>
       <div className="error-page__mid-title">
         <h1>{code}</h1>
       </div>
       <div className="error-page__bottom-title">
         {code === 404 && <h2>{t('errorPage.title3')}</h2>}
-        {code === 401 && <h2>{t('errorPage.title4')}</h2>}
+        {code === 403 && <h2>{t('errorPage.title4')}</h2>}
       </div>
       <div className="error-page__home-link">
         <Link to={"/"}>{t('errorPage.link1')}</Link>
