9d3d64857ddc1fbf6cb0d9c734b50e7fb6f0dc37
Konstantin Ashaev <kostya@gorgaz.ru>
2022-09-06 16:03:34 +0300

    Move handleDownload() to <RowBox />

--- src/Rows.js
+++ src/Rows.js
@@ -1,38 +1,12 @@
 import RowBox from './RowBox.js';
 import RowContacts from './RowContacts.js';
 
-function handleDownload(e, hash, name, token, dispatch) {
-  e.preventDefault();
-  const url = `/api/download/${hash}`;
-  fetch(url, { headers: { 'Authorization': `Bearer ${token}` }})
-    .then(res => {
-      if (res.status === 200) {
-        if (res.token) {
-          dispatch({ type: 'TOKEN', payload: { token: { string: res.token } }});
-        }
-      return res.blob();
-    }
-      if (res.status === 401) {
-        dispatch({ type: 'LOGIN', payload: { user: { loggedIn: false } }});
-        }
-    })
-    .then(blob => {
-      const objectURL = URL.createObjectURL(blob);
-      const a = document.createElement('a');
-      a.href = objectURL;
-      a.download = name;
-      a.click();
-      URL.revokeObjectURL(a.href);
-    })
-    .catch((e) => console.error(e))
-}
-
 const Rows = ({ rows, kind }) => {
   if (kind === 'box') {
     return (
       <tbody>
         {rows.map((row, index) => {
-          return <RowBox entry={row} key={index} getFile={handleDownload} />;
+          return <RowBox entry={row} key={index} />;
         })}
       </tbody>
     );
