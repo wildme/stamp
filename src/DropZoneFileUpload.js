6041e0e1b3dfeb778656ac7ca0a173c243bb30b7
Konstantin Ashaev <kostya@gorgaz.ru>
2022-11-18 15:43:06 +0300

    First commit

--- src/DropZoneFileUpload.js
+++ src/DropZoneFileUpload.js
@@ -1,34 +1,48 @@
-//import { useState } from 'react';
+import { useState, Fragment } from 'react';
 
 const DropZoneFileUpload = (props) => {
   const t = props.t;
   const setter = props.setter;
   const className = props.className;
+  const [filename, setFilename] = useState(null);
 
   const onDragOver = (e) => {
     e.preventDefault();
     const doc = document.querySelector(`.${className}`);
-    doc.classList.add(`${className}_dark`);
-
+    doc.classList.add(`${className}_active`);
   };
 
   const onDragLeave = (e) => {
     e.preventDefault();
     const doc = document.querySelector(`.${className}`);
-    doc.classList.remove(`${className}_dark`);
+    doc.classList.remove(`${className}_active`);
   }
 
   const onDrop = (e) => {
     e.preventDefault();
     e.stopPropagation();
-    const file = e.dataTransfer.files;
-    setter(file[0]);
+    const { files } = e.dataTransfer;
+    setFilename(files[0].name);
   };
   
   return (
-    <div>
-        <section className={`${className}__title`}>Drop file</section>
-    </div>
+    <Fragment>
+      <div
+        className={className}
+        onDrop={(e) => onDrop(e)}
+        onDragOver={(e) => onDragOver(e)}
+        onDragLeave={(e) => onDragLeave(e)}
+      >
+      {!filename &&
+        <section className={`${className}__title`}>
+          {t('dropZoneFile.title1')}
+        </section>}
+      {filename &&
+        <section className={`${className}__filename`}>
+          {filename}
+        </section>}
+      </div>
+    </Fragment>
   )
 };
 
