fd736e575bfeb56d03bd44541ba7fb8e6f2717b3
Konstantin Ashaev <kostya@gorgaz.ru>
2022-12-28 16:37:57 +0300

    First implementation

--- src/TableTabs.js
+++ src/TableTabs.js
@@ -1,23 +1,24 @@
-import { NavLink } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 const TableTabs = (props) => {
   const defaultValue = props.defaultValue || 'outbox';
   const queryString = props.queryString || '/letters?box=';
-  const className = props.className || 'box-tabs';
+  const className = props.className;
   const tabs = props.tabs || ['inbox', 'outbox'];
   const tabTitles = props.tabTitles || ['inbox', 'outbox'];
 
   return (
-    <div className={classname}>
-      <ul className=`${className}_list`>
-        <li>
-          <NavLink
-            className={`${className}__button`}
-            to={queryString}>
+    <div className={className}>
+          <Link
+            className={`${className}_link`}
+            to={queryString + 'inbox'}>
             Inbox
-          </NavLink>
-        </li>
-      </ul>
+          </Link>
+          <Link
+            className={`${className}_link`}
+            to={queryString + 'outbox'}>
+            Outbox
+          </Link>
     </div>
   );
 };
