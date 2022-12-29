944921d55cbe3a640c8c6bccebf90b29118f4eb0
Konstantin Ashaev <kostya@gorgaz.ru>
2022-12-29 16:20:37 +0300

    Replace links with buttons

--- src/TableTabs.js
+++ src/TableTabs.js
@@ -1,24 +1,31 @@
-import { Link } from 'react-router-dom';
-
 const TableTabs = (props) => {
+  const setter = props.setter;
   const defaultValue = props.defaultValue || 'outbox';
-  const queryString = props.queryString || '/letters?box=';
   const className = props.className;
   const tabs = props.tabs || ['inbox', 'outbox'];
   const tabTitles = props.tabTitles || ['inbox', 'outbox'];
+  const handleClick = (e) => {
+    let tabs = document.getElementsByClassName(`${className}__button`);
+    for (let i = 0; i < tabs.length; i++) {
+      tabs[i].className = tabs[i].className.replace(" active", "");
+    }
+    e.currentTarget.className += " active";
+  }
 
   return (
     <div className={className}>
-          <Link
-            className={`${className}_link`}
-            to={queryString + 'inbox'}>
-            Inbox
-          </Link>
-          <Link
-            className={`${className}_link`}
-            to={queryString + 'outbox'}>
-            Outbox
-          </Link>
+      <button
+        className={`${className}__button active`}
+        type="button"
+        value="inbox"
+        onClick={(e) => handleClick(e)}>Inbox
+      </button>
+      <button
+        className={`${className}__button`}
+        type="button"
+        value="outbox"
+        onClick={(e) => handleClick(e)}>Outbox
+      </button>
     </div>
   );
 };
