996bb3e366a6708eb8ef2b349e6c7c4fb01c7f8b
Konstantin Ashaev <kostya@gorgaz.ru>
2022-12-30 11:17:02 +0300

    Use expression to generate tabs

--- src/TableTabs.js
+++ src/TableTabs.js
@@ -1,31 +1,31 @@
 const TableTabs = (props) => {
-  const setter = props.setter;
-  const defaultValue = props.defaultValue || 'outbox';
+  const tabs = props.tabs;
+  const tabTitles = props.tabTitles;
+  const selectedTab = props.selectedTab;
   const className = props.className;
-  const tabs = props.tabs || ['inbox', 'outbox'];
-  const tabTitles = props.tabTitles || ['inbox', 'outbox'];
+  const setter = props.setter;
+
   const handleClick = (e) => {
     let tabs = document.getElementsByClassName(`${className}__button`);
     for (let i = 0; i < tabs.length; i++) {
       tabs[i].className = tabs[i].className.replace(" active", "");
     }
     e.currentTarget.className += " active";
+    setter(e.target.value);
   }
 
   return (
     <div className={className}>
+    {tabs.map((tab, i) => (
       <button
-        className={`${className}__button active`}
+        className={`${className}__button` +
+          (tab === selectedTab ? " active" : "")}
         type="button"
-        value="inbox"
-        onClick={(e) => handleClick(e)}>Inbox
+        value={tab}
+        key={i}
+        onClick={(e) => handleClick(e)}>{tabTitles[i]}
       </button>
-      <button
-        className={`${className}__button`}
-        type="button"
-        value="outbox"
-        onClick={(e) => handleClick(e)}>Outbox
-      </button>
+    ))}
     </div>
   );
 };
