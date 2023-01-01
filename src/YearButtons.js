1587f368b929af70c6dcb3a924132bcedc9e70b0
Konstantin Ashaev <techno_nix@mail.ru>
2023-01-02 00:31:02 +0300

    New file: YearButtons.js

---
+++ src/YearButtons.js
@@ -0,0 +1,31 @@
+const YearButtons = (props) => {
+  const containerClassName = props.containerClassName;
+  const buttonClassName = props.buttonClassName;
+  const activeButtonClassName = props.activeButtonClassName;
+  const currentYear = Number(props.currentYear);
+  const years = props.years;
+  const setter = props.setter;
+
+  const handleYearClick = (e) => {
+    const buttons = document.getElementsByClassName(buttonClassName);
+    for (let i = 0; i < buttons.length; i++) {
+      buttons[i].className = buttons[i].className.replace(activeButtonClassName, "");
+    }
+    e.currentTarget.className += ` ${activeButtonClassName}`;
+    setter(e.target.value);
+  }
+
+  return (
+    <div className={containerClassName}>
+      {years.map((item, i) => (
+        <button
+          key={i}
+          value={item}
+          className={buttonClassName}
+          onClick={(e) => handleYearClick(e)}>{item}
+        </button>))}
+    </div>
+  );
+};
+
+export default YearButtons;
