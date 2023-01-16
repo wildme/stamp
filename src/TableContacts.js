f9cba529a72acfdb02a4a93d0d780b1d90482f40
Konstantin Ashaev <kostya@gorgaz.ru>
2023-01-16 16:13:19 +0300

    New import. Pass a new prop to TableHead

--- src/TableContacts.js
+++ src/TableContacts.js
@@ -1,5 +1,6 @@
 import { createContext } from 'react';
 import TableHead from './TableHead.js';
+import ContactsTableColumnLabels  from './ContactsTableColumnLabels.js';
 import Rows from './Rows.js';
 
 export const ContactsContext = createContext();
@@ -10,14 +11,15 @@ const TableContacts = (props) => {
   const noDataMsg = props.noDataMsg;
   const setter = props.setInfoMsg;
   const className = props.className;
+  const labels = ContactsTableColumnLabels();
 
   return (
     <div className={className}>
       <table className={`${className}__table`}>
         <TableHead
+          labels={labels["contacts"]}
           trClassName={`${className}__tr`}
           thClassName={`${className}__th`}
-          table="contacts"
         />
         <ContactsContext.Provider value={setter}>
           {content &&
