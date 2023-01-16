3f13d66a12af6b3be8eb4003b9fca5f547930697
Konstantin Ashaev <kostya@gorgaz.ru>
2023-01-16 16:15:33 +0300

    Remove useTranslation(). Get column labels from props

--- src/TableHead.js
+++ src/TableHead.js
@@ -1,22 +1,19 @@
-import { useTranslation } from 'react-i18next';
-import { headerColumns } from './headerColumns.js';
 import SortIcon from './sortIcon.js';
 
 const TableHead = (props) => {
   const trClassName = props.trClassName;
   const thClassName = props.thClassName;
   const sortButtonClassName = props.sortButtonClassName;
-  const table = props.table;
   const handleClick = props.handleClick;
   const sortOrder = props.sortOrder;
   const column = props.column;
   const noData = props.noData;
-  const { t } = useTranslation();
+  const labels = props.labels;
 
   return (
     <thead>
       <tr className={trClassName}>
-        {headerColumns[table].map((item) => {
+        {labels.map((item) => {
           const currentItem = column === item.id;
           const direction = currentItem ? sortOrder : '';
 
@@ -27,8 +24,8 @@ const TableHead = (props) => {
                 disabled={noData}
                 type="button"
                 onClick={() => handleClick(item.id)}>
-                {t(item.label)} <SortIcon direction={direction} />
-              </button> : t(item.label)}
+                {item.label} <SortIcon direction={direction} />
+              </button> : item.label}
             </th>
           );
         })}
