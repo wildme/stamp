fc3ba06ee4248b8f5893ee13b50e06fd5152d829
Konstantin Ashaev <techno_nix@mail.ru>
2023-06-12 18:37:33 +0300

    New file: LogoBlankPage.js

---
+++ src/LogoBlankPage.js
@@ -0,0 +1,10 @@
+const LogoBlankPage = () => {
+  return (
+    <div className="logo-blank-page">
+      <div className="logo-wrapper logo-blank-page__logo-wrapper">
+      </div>
+    </div>
+  );
+};
+
+export default LogoBlankPage;
