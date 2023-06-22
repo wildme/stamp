80eee3cba4e6f78610726ea7a8bed8cb3e207b28
Konstantin Ashaev <kostya@gorgaz.ru>
2023-06-22 10:39:55 +0300

    New file: stampI18n.js

---
+++ src/stampI18n.js
@@ -0,0 +1,14 @@
+import i18next from './i18n/i18n.js';
+
+export const stampI18n = {
+  inbox: [
+    {
+      prefix: i18next.t('main.prefix1'),
+    }
+  ],
+  outbox: [
+    {
+      prefix: i18next.t('main.prefix2')
+    }
+  ]
+};
