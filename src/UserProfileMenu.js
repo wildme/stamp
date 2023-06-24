9970dbb5ac42c5f829d1e53e7c1960ece73a0d2c
Konstantin Ashaev <techno_nix@mail.ru>
2023-06-24 22:40:01 +0300

    Use map() to generate menu

--- src/UserProfileMenu.js
+++ src/UserProfileMenu.js
@@ -1,43 +1,22 @@
 import { NavLink } from 'react-router-dom';
-import { useTranslation } from 'react-i18next';
+import { stampMenu as menu } from './stampMenu.js';
 
 const UserProfileMenu = () => {
-  const { t } = useTranslation();
   return (
     <div className="user-profile__navbar">
       <ul className="user-profile__navbar_list">
-        <li>
-          <NavLink
-            className="user-profile__navbar_link"
-            end
-            to="/my-profile">
-            {t('userProfile.item1')}
-          </NavLink>
-        </li>
-        <li>
-          <NavLink
-            className="user-profile__navbar_link"
-            end
-            to="/my-profile/e-mail">
-            {t('userProfile.item2')}
-          </NavLink>
-        </li>
-        <li>
-          <NavLink
-            className="user-profile__navbar_link"
-            end
-            to="/my-profile/password">
-            {t('userProfile.item3')}
-          </NavLink>
-        </li>
-        <li>
-          <NavLink
-            className="user-profile__navbar_link"
-            end
-            to="/my-profile/settings">
-            {t('userProfile.item4')}
-          </NavLink>
-        </li>
+        {menu['userProfile'].map((link, i) => {
+          return (
+            <li key={i}>
+              <NavLink
+                className="user-profile__navbar_link"
+                end
+                to={link.to}>
+                {link.name}
+              </NavLink>
+            </li>
+          );
+        })}
       </ul>
     </div>
   );
