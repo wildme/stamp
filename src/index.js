60622a0c13912af64f8ecb036de7bf8722b0a94a
Konstantin Ashaev <kostya@gorgaz.ru>
2023-06-30 08:30:23 +0300

    Update code according to the new root API in React 18

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import App from './App.js';
@@ -6,10 +6,11 @@ import './index.css';
 import rootReducer from './redux/rootReducer';
 
 const store = createStore(rootReducer);
+const container = document.getElementById('root');
+const root = createRoot(container);
 
-ReactDOM.render(
+root.render(
   <Provider store={store}>
     <App />
-  </Provider>,
-  document.getElementById('root')
+  </Provider>
 );
