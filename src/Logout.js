0004f25ffceb636e0205fc926691969167658dca
Konstantin Ashaev <kostya@gorgaz.ru>
2023-08-21 13:28:04 +0300

    Replace useNavigate() with Navigate

--- src/Logout.js
+++ src/Logout.js
@@ -1,21 +1,19 @@
-import { useNavigate } from 'react-router-dom';
+import { Navigate } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 
 const Logout = () => {
   const dispatch = useDispatch();
-  const navigate = useNavigate();
 
   fetch("/api/logout")
     .then(res => {
       if (res.ok) {
-        navigate("/login");
         localStorage.removeItem('at');
         dispatch({ type: 'LOGOUT', payload: { user: { loggedIn: false },
           info: null, settings: null } });
       }
     })
     .catch((e) => console.error(e))
-  return null;
+  return <Navigate to="/login" replace />;
 };
 
 export default Logout;
